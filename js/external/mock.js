3bf7a99ca3f3eb5d83bac382e105388b33068aaa
macpie <peter@2600hz.com>
2012-10-10 11:12:29 -0700

    First Clean of the libraries

--- js/external/mock.js
+++
@@ -1,6 +0,0 @@
-$.mockjax('api/v2/authenticate/establish', {
-	proxy: 'module/auth/mock/auth.establish-success.json'
-});
-$.mockjax('api/v2/authenticate/destroy', {
-	proxy: 'module/auth/mock/auth.destroy-success.json'
-});
