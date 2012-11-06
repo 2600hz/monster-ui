3f2ae8730abdfc7b1092be3093467865205e82f2
macpie <peter@2600hz.com>
2012-11-06 11:17:06 -0800

    MONSTER-19: myaccount/profile working !

--- whapps/myaccount/profile/profile.js
+++ whapps/myaccount/profile/profile.js
@@ -16,7 +16,16 @@ winkstart.module('myaccount', 'profile', {
         },
 
         resources: {
-            
+            'profile.user_get': {
+                url: '{api_url}/accounts/{account_id}/users/{user_id}',
+                contentType: 'application/json',
+                verb: 'GET'
+            },
+            'profile.user_update': {
+                url: '{api_url}/accounts/{account_id}/users/{user_id}',
+                contentType: 'application/json',
+                verb: 'POST'
+            }
         }
     },
 
@@ -26,14 +35,75 @@ winkstart.module('myaccount', 'profile', {
         winkstart.registerResources(THIS.__whapp, THIS.config.resources);
     },
     {
+        user_update: function(data, new_data, success, error) {
+            winkstart.request('profile.user_update', {
+                    account_id: winkstart.apps['myaccount'].account_id,
+                    api_url: winkstart.apps['myaccount'].api_url,
+                    user_id: winkstart.apps['myaccount'].user_id,
+                    data: $.extend(true, {}, data, new_data)
+                },
+                function(_data, status) {
+                    if(typeof success == 'function') {
+                        success(_data, status);
+                    }
+                },
+                function(_data, status) {
+                    if(typeof error == 'function') {
+                        error(_data, status);
+                    }
+                }
+            );
+        },
+
         render: function() {
-            var THIS = this,
-                $profile_html = THIS.templates.profile.tmpl();
+            var THIS = this;
 
-            winkstart.publish('myaccount.select_menu', THIS.__module);
+            winkstart.request('profile.user_get', {
+                account_id: winkstart.apps['myaccount'].account_id,
+                api_url: winkstart.apps['myaccount'].api_url,
+                user_id: winkstart.apps['myaccount'].user_id
+            },
+            function(data, status) {
+                var $profile_html = THIS.templates.profile.tmpl({
+                    'email': (data.data.email) ? data.data.email : ""
+                });
 
-            $('.myaccount .myaccount-content .container-fluid').html($profile_html); 
+                $('.change-email', $profile_html).on('click', function(e) {
+                    e.preventDefault();
 
+                    THIS.user_update(data.data, {
+                        email: $('#email', $profile_html).val()
+                    },
+                    function() {
+                        alert('Email address updated!');
+                    });
+                });
+
+
+                $('.change-password', $profile_html).click(function(e) {
+                    e.preventDefault();
+
+                    var pass = $('#password', $profile_html).val(),
+                        confirm_pass = $('#confirm_password', $profile_html).val()
+
+                    if(pass == confirm_pass) {
+                        if(winkstart.is_password_valid(pass)) {
+                            THIS.user_update(data.data, {
+                                    password: pass
+                                },
+                                function() {
+                                    alert('Password updated!');
+                                }
+                            );
+                        }
+                    } else {
+                        alert('Passwords do not match, please retype the passwords.');
+                    }
+                });
+
+                winkstart.publish('myaccount.select_menu', THIS.__module);
+                $('.myaccount .myaccount-content .container-fluid').html($profile_html); 
+            });
         },
 
         myaccount_loaded: function($myaccount_html) {
