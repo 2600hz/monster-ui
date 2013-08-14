e4960508259381d33c43645c01d645711e62f9eb
Jean-Roch Maitre <jr@2600hz.com>
2013-08-13 18:32:19 -0700

    MON-78: Fixed relative path for requireJS

--- apps/common/app.js
+++ apps/common/app.js
@@ -2,7 +2,7 @@ define(function(require){
 	var $ = require('jquery'),
 		_ = require('underscore'),
 		monster = require('monster'),
-		accountDropdown = require('apps/common/js/accountDropdown');
+		accountDropdown = require('./js/accountDropdown');
 
 	var app = {
 
