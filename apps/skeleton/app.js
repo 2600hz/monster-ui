ab589bd5fdb8b6b0c5bca6b587a8e520ba154453
Jean-Roch Maitre <jr@2600hz.com>
2013-10-29 01:52:08 -0700

    KAZOO-1607: Added a skeleton app to the repo.

---
+++ apps/skeleton/app.js
@@ -0,0 +1,103 @@
+define(function(require){
+	var $ = require('jquery'),
+		_ = require('underscore'),
+		monster = require('monster'),
+		toastr = require('toastr');
+
+	var app = {
+
+		name: 'skeleton',
+
+		i18n: [ 'en-US' ],
+
+		requests: {
+			/* This will use the default api url given in js/config.js. If you want to use your own api you can use the following
+
+			'skeleton.listNumbers': {
+				apiRoot: 'http://yourapiurl/',
+				url: 'phone_numbers?prefix={pattern}&quantity={size}',
+				verb: 'GET'
+			}
+
+			*/
+			'skeleton.listNumbers': {
+				url: 'phone_numbers?prefix={pattern}&quantity={size}',
+				verb: 'GET'
+			}
+		},
+
+		subscribe: {
+			'pbxsManager.activate': '_render'
+		},
+
+		load: function(callback){
+			var self = this;
+
+			self.initApp(function() {
+				callback && callback(self);
+			});
+		},
+
+		initApp: function(callback) {
+			var self = this;
+
+			/* Used to init the auth token and account id */
+			monster.pub('auth.initApp', {
+				app: self,
+				callback: callback
+			});
+		},
+
+		render: function(container){
+			var self = this;
+
+			self._render(container);
+		},
+
+		// subscription handlers
+		_render: function(container) {
+			var self = this,
+				skeletonTemplate = $(monster.template(self, 'layout')),
+				parent = _.isEmpty(container) ? $('#ws-content') : container;
+
+			self.bindEvents(skeletonTemplate);
+
+			(parent)
+				.empty()
+				.append(skeletonTemplate);
+		},
+
+		bindEvents: function(template) {
+			var self = this;
+
+			template.find('#search').on('click', function(e) {
+				self.searchNumbers(415, 15, function(listNumbers) {
+					var results = monster.template(self, 'results', listNumbers);
+
+					template
+						.find('.results')
+						.empty()
+						.append(results);
+				});
+			});
+		},
+
+		//utils
+		searchNumbers: function(pattern, size, callback) {
+			var self = this;
+
+			monster.request({
+				resource: 'skeleton.listNumbers',
+				data: {
+					pattern: pattern,
+					size: size
+				},
+				success: function(listNumbers) {
+					callback && callback(listNumbers.data);
+				}
+			});
+		}
+	};
+
+	return app;
+});
