e3f81bfb7964609de46357c639bf09a84b1fbf0a
Joris Tirado <joris.tirado@gmail.com>
2014-04-14 16:55:26 -0700

    KAZOO-2298: Create password recovery popup

--- apps/auth/app.js
+++ apps/auth/app.js
@@ -34,6 +34,11 @@ define(function(require){
 			'auth.getAccount': {
 				url: 'accounts/{accountId}',
 				verb: 'GET'
+			},
+			'auth.recover_password': {
+				url: 'user_auth/recovery',
+				contentType: 'application/json',
+				verb: 'PUT'
 			}
 		},
 
@@ -277,6 +282,33 @@ define(function(require){
 			content.empty().append(loginHtml);
 
 			content.find(templateData.username !== '' ? '#password' : '#login').focus();
+
+			content.find('.forgot-password').on('click', function() {
+				var	template = $(monster.template(self, 'dialogPasswordRecovery')),
+					dialog;
+
+				template.find('.recover-password').on('click', function() {
+					var object = form2object('form2object');
+
+					object.account_name === '' ? delete object.account_name : true;
+					object.account_realm === '' ? delete object.account_realm : true;
+					object.phone_number === '' ? delete object.phone_number : true;
+
+					console.log(object);
+
+					monster.request({
+						resource: 'auth.recover_password',
+						data: {
+							data: object
+						},
+						success: function(data, status) {
+							console.log(data, status);
+						}
+					});
+				});
+
+				dialog = monster.ui.dialog(template, { title: 'Password Recovery' });
+			});
 
 			content.find('.login').on('click', function(event){
 				event.preventDefault();
