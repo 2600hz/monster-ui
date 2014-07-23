7822c451c5275876bb68af3d5513cd97f0cf9cdc
Jean-Roch Maitre <jr@2600hz.com>
2014-07-23 15:09:57 -0700

    KAZOO-2799: All the require dependicies are now written with quotes to be consistent.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,61 @@
 
 require.config({
 	paths: {
-		async: 'js/lib/async',
-		bootstrap: 'js/lib/bootstrap-2.3.1.min',
-		'bootstrap-switch': 'js/lib/bootstrapSwitch-1.8.min',
+		'async': 'js/lib/async',
+		'bootstrap': 'js/lib/bootstrap-2.3.1.min',
 		'bootstrap-clickover': 'js/lib/bootstrapx-clickover-1.0',
-		chart: 'js/lib/chart.min',
-		chosen: 'js/lib/jquery.chosen.min',
-		config: 'js/config',
-		datatables: 'js/lib/tables/jquery.dataTables-1.8',
-		ddslick: 'js/lib/jquery.ddslick.min',
-		footable: 'js/lib/footable/footable.min',
-		'footable-sort': 'js/lib/footable/footable.sort.min',
+		'bootstrap-switch': 'js/lib/bootstrapSwitch-1.8.min',
+		'chart': 'js/lib/chart.min',
+		'chosen': 'js/lib/jquery.chosen.min',
+		'config': 'js/config',
+		'datatables': 'js/lib/tables/jquery.dataTables-1.8',
+		'ddslick': 'js/lib/jquery.ddslick.min',
+		'footable': 'js/lib/footable/footable.min',
 		'footable-filter': 'js/lib/footable/footable.filter.min',
-		form2object: 'js/lib/form2object',
-		handlebars: 'js/lib/handlebars-1.0.10',
-		hotkeys: 'js/lib/jquery.hotkeys.min',
-		icheck: 'js/lib/jquery.icheck.min',
-		isotope: 'js/lib/jquery.isotope.min',
-		jquery: 'js/lib/jquery-1.9.1.min',
-		jqueryui: 'js/lib/jquery-ui-1.10.3.custom.min',
-		'touch-punch': 'js/lib/jquery.ui.touch-punch.min',
-		jstz: 'js/lib/jstz.min',
-		kazoosdk: 'js/lib/jquery.kazoosdk',
-		leaflet: 'js/lib/leaflet.min',
-		mask: 'js/lib/jquery.mask',
-		monster: 'js/lib/monster',
+		'footable-sort': 'js/lib/footable/footable.sort.min',
+		'form2object': 'js/lib/form2object',
+		'handlebars': 'js/lib/handlebars-1.0.10',
+		'hotkeys': 'js/lib/jquery.hotkeys.min',
+		'icheck': 'js/lib/jquery.icheck.min',
+		'isotope': 'js/lib/jquery.isotope.min',
+		'jquery': 'js/lib/jquery-1.9.1.min',
+		'jqueryui': 'js/lib/jquery-ui-1.10.3.custom.min',
+		'jstz': 'js/lib/jstz.min',
+		'kazoosdk': 'js/lib/jquery.kazoosdk',
+		'leaflet': 'js/lib/leaflet.min',
+		'mask': 'js/lib/jquery.mask',
+		'monster': 'js/lib/monster',
 		'monster-ui': 'js/lib/monster.ui',
 		'monster-timezone': 'js/lib/monster.timezone',
 		'monster-util': 'js/lib/monster.util',
-		nicescroll: 'js/lib/jquery.nicescroll.min',
+		'nicescroll': 'js/lib/jquery.nicescroll.min',
 		'monster-apps': 'js/lib/monster.apps',
-		plugins: 'js/plugins',
-		postal: 'js/lib/postal-0.8.2',
-		reqwest: 'js/lib/reqwest-0.7.3.min',
-		toastr: 'js/lib/toastr-1.3.0',
-		timepicker: 'js/lib/jquery.timepicker.min',
-		socket: 'js/lib/socket.io.min',
-		underscore: 'js/lib/underscore-1.4.4.min',
-		validate: 'js/lib/jquery.validate.min',
-		wysiwyg: 'js/lib/bootstrap.wysiwyg.min'
+		'plugins': 'js/plugins',
+		'postal': 'js/lib/postal-0.8.2',
+		'reqwest': 'js/lib/reqwest-0.7.3.min',
+		'socket': 'js/lib/socket.io.min',
+		'timepicker': 'js/lib/jquery.timepicker.min',
+		'toastr': 'js/lib/toastr-1.3.0',
+		'touch-punch': 'js/lib/jquery.ui.touch-punch.min',
+		'underscore': 'js/lib/underscore-1.4.4.min',
+		'validate': 'js/lib/jquery.validate.min',
+		'wysiwyg': 'js/lib/bootstrap.wysiwyg.min'
 	},
 	shim: {
 		'footable-sort': ['footable'],
 		'footable-filter': ['footable'],
-		bootstrap: ['jqueryui'],
+		'bootstrap': ['jqueryui'],
 		'bootstrap-switch': ['bootstrap'],
 		'bootstrap-clickover': ['bootstrap'],
-		datatables: ['jquery'],
-		jqueryui: ['jquery'],
-		handlebars: {
-			exports: 'Handlebars'
+		'datatables': ['jquery'],
+		'jqueryui': ['jquery'],
+		'handlebars': {
+			'exports': 'Handlebars'
 		},
-		plugins: ['jquery'],
+		'plugins': ['jquery'],
 		'touch-punch': ['jqueryui'],
-		underscore: {
-			exports: '_'
+		'underscore': {
+			'exports': '_'
 		}
 	},
 	urlArgs: 'bust=' + (new Date()).getTime()
@@ -75,10 +75,9 @@ require(['jquery', 'monster', 'plugins', 'bootstrap', 'bootstrap-switch', 'boots
 			monster.socket = io.connect(monster.config.api.socket);
 		}
 
-		$(function(){
-			monster.apps.load('core', function(app){
-				app.render($('#main'));
-			});
+		monster.apps.load('core', function(app){
+			app.render($('#main'));
 		});
 	});
 });
+
