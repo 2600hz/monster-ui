f239f8d1cd456d61aa9dfbd1fceb0cb6fe65b59d
Jean-Roch Maitre <jr@2600hz.com>
2014-10-10 15:26:52 -0700

    UI-769: Fixed config.js

--- js/config.js
+++ js/config.js
@@ -2,24 +2,23 @@ define(function(require){
 
 	return {
 		api: {
-			provisioner: 'http://10.26.0.115:8888/Provisioner-2600hz/',
-			default: 'http://10.26.0.41:8000/v2/'/*,*/
-			// default: 'http://10.26.0.61:8000/v2/
-			//socket: 'http://10.26.0.41:5555'
+			default: 'http://api.zswitch.net:8000/v2/'
+			// If you have provisioner turned on in your install and can use the one provided by 2600hz, add the URL in the 'provisioner' key below
+			// provisioner: 'your_provisioner_url',
+			// If you want to use WebSockets you need to turn blackhole on in the back-end and then put the URL in the 'socket' key below
+			// socket: 'your_web_socket_url'
 		},
 
-		//language: 'en-US',
-		resellerId: '64cd8d2a12ac216104a1341ab1b9ab2a',
+		// By default the language is set by the browser, and once the user is log in it will take what's set in the account/user.
+		// If you want to force the language of the UI before the user is logged in, you can set it here.
+		// language: 'en-US',
 
-		company: {
-			name: '2600hz',
-			website: ''
-		},
+		// The resellerId key is the accountId of your master account, and is needed for some reseller features
+		// For example it won't prompt for a credit card the subaccounts that have a different resellerId than this resellerId
+		// resellerId: 'your_master_account_id',
 
 		nav: {
-			help: 'http://wiki.2600hz.com',
-			learnMore: 'http://www.2600hz.com/'
+			help: 'http://wiki.2600hz.com'
 		}
 	};
-
 });
