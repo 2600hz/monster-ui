68a060d80b8eb66f0949dfb0d50a6f00bca1fb5e
Joris Tirado <joris.tirado@gmail.com>
2015-01-09 16:28:21 -0800

    UI-994: Add regex rule for the validation

--- js/lib/monster.ui.js
+++ js/lib/monster.ui.js
@@ -757,6 +757,11 @@ define(function(require){
 				else return true;
 			}, localization.customRules['checkList']);
 
+			// Adding advanced custom rules
+			$.validator.addMethod('regex', function(value, element, regexpr) {
+				return regexpr.test(value);
+			});
+
 			this.customValidationInitialized = true;
 		},
 
