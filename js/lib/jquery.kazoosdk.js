62fa13f1476996dc00c8b7e4238988b7db8e3ef9
Jean-Roch Maitre <jr@2600hz.com>
2015-02-04 14:19:26 -0800

    UI-1119: Added new APIs needed for the Kazoo UI to SmartPBX migration, + now feeding the global handler so the UI can handle special error cases

--- js/lib/jquery.kazoosdk.js
+++ js/lib/jquery.kazoosdk.js
@@ -124,7 +124,9 @@
 			'create': { verb: 'PUT', url: 'accounts/{accountId}/callflows' },
 			'update': { verb: 'POST', url: 'accounts/{accountId}/callflows/{callflowId}' },
 			'delete': { verb: 'DELETE', url: 'accounts/{accountId}/callflows/{callflowId}' },
-			'list': { verb: 'GET', url: 'accounts/{accountId}/callflows' }
+			'list': { verb: 'GET', url: 'accounts/{accountId}/callflows' },
+			'searchByNameAndNumber': { verb: 'GET', url: 'accounts/{accountId}/search?t=callflow&q=name_and_number&v={value}'},
+			'searchByNumber': { verb: 'GET', url: 'accounts/{accountId}/search?t=callflow&q=number&v={value}'}
 		},
 		cdrs: {
 			'get': { verb: 'GET', url: 'accounts/{accountId}/cdrs/{cdrId}' },
@@ -397,7 +399,7 @@
 
 			options.onRequestError && options.onRequestError(error, options);
 
-			options.error && options.error(parsedError, error);
+			options.error && options.error(parsedError, error, options.onRequestError);
 		};
 
 		settings.success = function requestSuccess(responseData, status, jqXHR) {
@@ -405,7 +407,7 @@
 
 			options.onRequestSuccess && options.onRequestSuccess(responseData, options);
 
-			options.success && options.success(responseData);
+			options.success && options.success(responseData, options.onRequestSuccess);
 		};
 
 		settings.url = settings.url.replace(rurlData, function (m, key) {
