2ede29bb53484714f4a098defc4f9b6e897e898d
Jean-Roch Maitre <jr@2600hz.com>
2015-04-24 16:06:50 -0700

    UI-901: created control to show 2 column results (success/errors) used in carriers and new phone number delete

--- js/lib/monster.ui.js
+++ js/lib/monster.ui.js
@@ -1402,6 +1402,14 @@ define(function(require){
 			} else {
 				throw 'You must provide at least one input field';
 			}
+		},
+
+		results: function(data) {
+			var self = this,
+				i18n = monster.apps.core.i18n.active(),
+				template = monster.template(monster.apps.core, 'monster-results', data);
+
+			return template;
 		}
 	};
 
