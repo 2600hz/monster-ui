9e348b839c0dc4c5fcf05afa3790c9e71297da9e
Jean-Roch Maitre <jr@2600hz.com>
2015-06-30 17:24:08 -0700

    UI-1579: Added helper to get next extension, as it's used in smartpbx and the migration app

--- js/lib/monster.util.js
+++ js/lib/monster.util.js
@@ -650,6 +650,55 @@ define(function(require){
 			}
 
 			return result;
+		},
+
+		/* Helper function that takes an array of number in parameter, sorts it, and returns the first number not in the array, greater than the minVal */
+		getNextExtension: function(listNumbers) {
+			var orderedArray = listNumbers,
+				previousIterationNumber,
+				minNumber = 1000,
+				lowestNumber = minNumber,
+				increment = 1;
+
+			orderedArray.sort(function(a,b) {
+				var parsedA = parseInt(a),
+					parsedB = parseInt(b);
+
+				if(isNaN(parsedA)) {
+					return -1;
+				}
+				else if(isNaN(parsedB)) {
+					return 1;
+				}
+				else {
+					return parsedA > parsedB ? 1 : -1;
+				}
+			});
+			console.log(orderedArray);
+			_.each(orderedArray, function(number) {
+				var currentNumber = parseInt(number);
+
+				// First we make sure it's a valid number, if not we move on to the next number
+				if(!isNaN(currentNumber)) {
+					// If we went through this loop already, previousIterationNumber will be set to the number of the previous iteration
+					if(typeof previousIterationNumber !== 'undefined') {
+						// If there's a gap for a number between the last number and the current number, we check if it's a valid possible number (ie, greater than minNumber)
+						// And If yes, we return it, if not we just continue
+						if(currentNumber - previousIterationNumber !== increment && previousIterationNumber >= minNumber) {
+							return previousIterationNumber + increment;
+						}
+					}
+					// else, it's the first iteration, we initialize the minValue to the first number in the ordered array
+					// only if it's greater than 1000, because we don't want to recommend lower numbers
+					else if(currentNumber > minNumber) {
+						lowestNumber = currentNumber;
+					}
+					// We store current as the previous number for the next iteration
+					previousIterationNumber = currentNumber;
+				}
+			});
+
+			return (previousIterationNumber) ? previousIterationNumber + increment : lowestNumber;
 		}
 	};
 
