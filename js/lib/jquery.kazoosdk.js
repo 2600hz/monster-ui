48004b78ae60aefcabe243603be17e3c122c1b97
Jean-Roch Maitre <jr@2600hz.com>
2015-07-06 16:19:36 -0700

    UI-377: Dependencies in core for addition of restart button in SmartPBX

--- js/lib/jquery.kazoosdk.js
+++ js/lib/jquery.kazoosdk.js
@@ -179,7 +179,8 @@
 			'delete': { verb: 'DELETE', url: 'accounts/{accountId}/devices/{deviceId}' },
 			'list': { verb: 'GET', url: 'accounts/{accountId}/devices' },
 			'getStatus': { verb: 'GET', url: 'accounts/{accountId}/devices/status' },
-			'quickcall': { verb: 'GET', url: 'accounts/{accountId}/users/{deviceId}/quickcall/{number}'}
+			'quickcall': { verb: 'GET', url: 'accounts/{accountId}/users/{deviceId}/quickcall/{number}'},
+			'restart': { verb: 'POST', url: 'accounts/{accountId}/users/{deviceId}/sync'}
 		},
 		media: {
 			'get': { verb: 'GET', url: 'accounts/{accountId}/media/{mediaId}' },
