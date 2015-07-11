35b001690ae37f7f936973c8e924ed87b5bbee12
karl anderson <karl@2600hz.com>
2015-07-10 18:07:46 -0700

    Fixing conflicts

--- js/lib/jquery.kazoosdk.js
+++ js/lib/jquery.kazoosdk.js
@@ -175,7 +175,7 @@
 			'delete': { verb: 'DELETE', url: 'accounts/{accountId}/devices/{deviceId}' },
 			'list': { verb: 'GET', url: 'accounts/{accountId}/devices' },
 			'getStatus': { verb: 'GET', url: 'accounts/{accountId}/devices/status' },
-			'quickcall': { verb: 'GET', url: 'accounts/{accountId}/users/{deviceId}/quickcall/{number}'}
+			'quickcall': { verb: 'GET', url: 'accounts/{accountId}/devices/{deviceId}/quickcall/{number}'}
 		},
 		media: {
 			'get': { verb: 'GET', url: 'accounts/{accountId}/media/{mediaId}' },
