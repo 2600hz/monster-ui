d0e9495030edf9dfe457ad2b1a465ea28071f2e6
Jean-Roch Maitre <jr@2600hz.com>
2015-07-29 09:58:39 -0700

    CM-144: Added helper in handlebar to use the formatVariableToDisplay util helper

--- js/lib/monster.ui.js
+++ js/lib/monster.ui.js
@@ -29,6 +29,10 @@ define(function(require){
 		return monster.util.formatPhoneNumber(phoneNumber);
 	});
 
+	Handlebars.registerHelper('formatVariableToDisplay', function(variable) {
+		return monster.util.formatVariableToDisplay(variable);
+	});
+
 	Handlebars.registerHelper('toLowerCase', function(stringValue) {
 		return stringValue.toString().toLowerCase();
 	});
