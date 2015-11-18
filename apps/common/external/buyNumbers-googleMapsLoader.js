9348a9e35f360c68140e72943354413ca134125f
Joris Tirado <joris.tirado@gmail.com>
2015-11-18 14:25:03 -0800

    OOB: When loading Google Maps JavaScript Library, remove the sensor parameter that is no longer required for the Google Maps JavaScript API

--- apps/common/external/buyNumbers-googleMapsLoader.js
+++ apps/common/external/buyNumbers-googleMapsLoader.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	if (!window.hasOwnProperty('google')) {
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
-		script.src  = '//maps.google.com/maps/api/js?v=3&sensor=true&callback=gmap_draw';
+		script.src  = '//maps.google.com/maps/api/js?v=3&callback=gmap_draw';
 		window.gmap_draw = function() {};
 		$("head").append(script);
 	}
