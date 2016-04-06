2821a61d2adc8d43d821cc65c40f90de589c599e
Joris Tirado <joris.tirado@gmail.com>
2016-04-06 09:56:27 -0700

    Revert "OOB: Update Handlebars from v1.0.10 to v4.0.5"
    This reverts commit 51e1ac2d7bcac0a5590b576d057fda7c42a27b73.

--- js/lib/monster.ui.js
+++ js/lib/monster.ui.js
@@ -2,7 +2,6 @@ define(function(require){
 	var $ = require('jquery'),
 		_ = require('underscore'),
 		monster = require('monster'),
-		Handlebars = require('handlebars'),
 		hotkeys = require('hotkeys'),
 		toastr = require('toastr'),
 		validate = require('validate'),
