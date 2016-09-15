ed0ad0287debe435f476da0e98ee8861b06c0dde
Jean-Roch Maitre <odeonn@gmail.com>
2016-09-15 15:34:35 -0700

    working but dirty version of the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 // Plugins
-var gulp = require('gulp'),
+var fs = require('fs'),
+	path = require('path'),
+	gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	clean = require('gulp-clean'),
 	livereload = require('gulp-livereload'),
@@ -16,6 +18,27 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	uglify = require('gulp-uglify');
 
+//require('require-dir')('./gulp-tasks');
+
+function getDirectories(srcpath) {
+	return fs.readdirSync(srcpath).filter(function(file) {
+		return fs.statSync(path.join(srcpath, file)).isDirectory();
+	});
+}
+
+function getAppsToInclude() {
+	var apps = getDirectories('./src/apps'),
+		appsToExclude = ['tutorial', 'skeleton', 'demo_done'],
+		filteredApps = [];
+
+	for(var i in apps) {
+		if(appsToExclude.indexOf(apps[i]) < 0) {
+			filteredApps.push(apps[i]);
+		}
+	}
+	return filteredApps;
+}
+
 // Constants
 var _dist_path = './dist',
 	_dist_require_path = './distRequired',
@@ -23,63 +46,20 @@ var _dist_path = './dist',
 	_scss_path = './src/**/*.scss';
 
 var _require_js_paths = [],
-	_concat_css_path = [
-		'./dist/css/style.css'
-	],
-	coreApps = [
-		// core apps
-		'core',
-		'auth',
-		'common',
-		'myaccount',
-		'apploader',
-		'appstore'
-	],
-	otherApps = [
-		// apps
-		'accounts',
-		'branding',
-		'callflows',
-		'carriers',
-		'cluster',
-		'conferences',
-		'debug',
-		'dialplans',
-		'fax',
-		'inspector',
-		'migration',
-		'mobile',
-		'numbers',
-		'pbxs',
-		'pivot',
-		'port',
-		'provisioner',
-		'tasks',
-		'userportal',
-		'voicemails',
-		'voip',
-		'webhooks',
-		'websockets'
-	],
-	appsToInclude = coreApps.concat(otherApps);
+	_concat_css_path = ['./dist/css/style.css'],
+	_templates_html_path = [],
+	appsToInclude = getAppsToInclude();
 
-function getAppsToInclude() {
-	var apps = [];
+buildPaths();
 
-	if(gutil.env.env === 'prod') {
-		apps = coreApps.concat(otherApps);
+function buildPaths(prefix) {
+	for(var i in appsToInclude) {
+		_require_js_paths.push('apps/' + appsToInclude[i] + '/app');
+		_concat_css_path.push('./dist/apps/'+ appsToInclude[i] +'/style/*.css');
+		_templates_html_path.push('./src/apps/' + appsToInclude[i] + '/views/*.html');
 	}
-
-	return apps;
 }
 
-for(var i in appsToInclude) {
-	_require_js_paths.push('apps/' + appsToInclude[i] + '/app');
-}
-
-for(var i in appsToInclude) {
-	_concat_css_path.push('./dist/apps/'+ appsToInclude[i] +'/style/*.css');
-}
 
 gulp.task('concatCss', function() {
 	return gulp.src(_concat_css_path)
@@ -119,6 +99,13 @@ gulp.task('minifyJS', function() {
 		.pipe(gulp.dest('./dist/js/'));
 });
 
+gulp.task('minify-js-app', function() {
+	var app = gutil.env.app;
+	return gulp.src('./dist/apps/'+app+'/app.js')
+			.pipe(uglify())
+			.pipe(gulp.dest('./dist/apps/'+app));
+});
+
 var requireConfig = {  
 	dir: 'distRequired',
 	appDir: 'dist/',
@@ -139,6 +126,53 @@ var requireConfig = {
 			include: _require_js_paths 
 		}
 	]
+};
+
+var getConfigRequire = function(mode, app) {
+	var librariesToExclude = ['async','bootstrap','bootstrap-clickover','chart','card','chosen','crossroads','config','datatables','dependClass','ddslick','fileupload','footable','form2object','handlebars','hasher','hotkeys','introJs','isotope','jquery','jqueryui','jstz','kazoosdk','mask','modernizr','monster','monster-apps','monster-ui','monster-timezone','monster-routing','monster-ui','monster-util','mousetrap','nicescroll','plugins','papaparse','postal','prettify','renderjson','reqwest','signals','slider','timepicker','toastr','touch-punch','underscore','validate','vkbeautify','wysiwyg','pdfjs-dist/build/pdf','pdfjs-dist/build/pdf.worker', 'templates'];
+	if(mode === 'app') {
+		modules = [
+			{
+				name:'js/main',
+				exclude:[
+					'config',
+					'templates'
+				]
+			},
+			{
+				name: 'apps/' + app + '/app',
+				exclude: librariesToExclude
+			}
+		];
+	}
+	else if(mode === 'whole') {
+		modules = [
+			{
+				name:'js/main',
+				exclude:[
+					'config',
+					'templates'
+				],
+				include: _require_js_paths
+			}
+		];
+	}
+
+	var config = {  
+		dir: 'distRequired',
+		appDir: 'dist/',
+		baseUrl:'./',
+		mainConfigFile:'dist/js/main.js',
+		fileExclusionRegExp: /^doc*|.*\.md|^\..*|^monster-ui\.build\.js$/,
+		findNestedDependencies:true,
+		preserveLicenseComments:false,
+		removeCombined:true,
+		optimize: 'none', // prevent optimization because we don't want to minify config.js and there's no way to single it out // we should optimize with gulp later
+		modules: modules
+	};
+	console.log(config);
+
+	return config;
 };
 
 // Tasks
@@ -182,10 +216,20 @@ gulp.task('sass', function() {
 
 gulp.task('require', function(cb) {
 	runSequence('buildRequire', 'move-require', 'clean-require', cb);
-})
+});
+
+gulp.task('require-app', function(cb) {
+	runSequence('buildRequireApp', 'move-require', 'clean-require', cb);
+});
 
 gulp.task('buildRequire', function(cb){
-	rjs.optimize(requireConfig, function(buildResponse){
+	rjs.optimize(getConfigRequire('whole'), function(buildResponse){
+		cb();
+	}, cb);
+});
+
+gulp.task('buildRequireApp', function(cb){
+	rjs.optimize(getConfigRequire('app', gutil.env.app), function(buildResponse){
 		cb();
 	}, cb);
 });
@@ -200,6 +244,90 @@ gulp.task('move-dist-dev', ['clean-dist-dev'], function() {
 			.pipe(gulp.dest('dist-dev'));
 });
 
+gulp.task('compileTemplates', function(){
+	var paths = {
+			app: {
+				src: './src/apps/'+gutil.env.app+'/views/*.html',
+				dest: './dist/apps/'+gutil.env.app+'/views',
+				concatName: 'templates.js'
+			},
+			whole: {
+				src: _templates_html_path,
+				dest: 'tmp/js/',
+				concatName: 'templates-compiled.js'
+			}
+		},
+		mode = gutil.env.app ? 'app' : 'whole';
+
+	return gulp.src(paths[mode].src)
+		.pipe(handlebars({
+			handlebars: require('handlebars')
+		}))
+		.pipe(wrap('Handlebars.template(<%= contents %>)'))
+		.pipe(declare({
+			namespace: 'monster.cache.templates',
+			noRedeclare: true, // Avoid duplicate declarations ,
+			processName: function(filePath) {
+				var splits = filePath.split('\\'),
+					// our files are all in folder such as apps/accounts/views/test.html, so we want to extract the last and 2 before last parts to have the app name and the template name
+					newName = splits[splits.length - 3] +'.' + splits[splits.length-1];
+				return declare.processNameByPath(newName);
+			}
+		}))
+		.pipe(concat(paths[mode].concatName))
+		.pipe(gulp.dest(paths[mode].dest));
+});
+
+gulp.task('templates-app', function(cb) {
+	runSequence('compileTemplates', 'concat-js-app', 'clean-templates-app', cb);
+});
+
+gulp.task('concat-js-app', function() {
+	return gulp.src(['dist/apps/'+ gutil.env.app +'/app.js', 'dist/apps/'+ gutil.env.app +'/views/templates.js'])
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('dist/apps/'+ gutil.env.app +'/'));
+});
+
+gulp.task('clean-templates-app', function() {
+	return gulp.src(['./dist/apps/'+gutil.env.app+'/views/*.html', './dist/apps/'+ gutil.env.app+'/views/templates.js'], {read: false})
+		.pipe(clean());
+});
+
+gulp.task('templates', function(cb) {
+	runSequence('removeDistTemplates', 'compileTemplates', 'concatTemplates', 'clean-tmp', cb);
+});
+
+gulp.task('concatTemplates', function() {
+	return gulp.src(['src/js/templates.js', 'tmp/js/templates-compiled.js'])
+		.pipe(concat('templates.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('removeDistTemplates', function() {
+	return gulp.src('./dist/js/templates.js').pipe(clean());
+});
+
+// watch our scss files so that when we change a variable it reloads the browser with the new css
+gulp.task('watch', function (){
+	runSequence('build', function() {
+		livereload.listen();
+
+		gulp.watch(_scss_path, ['sass']);
+	});
+});
+
+gulp.task('write-config', function() {
+	fs.writeFileSync('build-config.json', '{}');
+
+	return gulp.src('build-config.json')
+			.pipe(jeditor({
+				preloadedApps: gutil.env.env === 'prod' ? getAppsToInclude() : []
+			}))
+			.pipe(gulp.dest('./dist'));
+});
+
+
 gulp.task('build-all', function(cb) {
 	runSequence( 
 		'build-dev',
@@ -207,7 +335,7 @@ gulp.task('build-all', function(cb) {
 		'build-prod',
 		cb
 	);
-})
+});
 
 gulp.task('build-dev', function(cb) {
 	gutil.env.env = 'dev';
@@ -236,9 +364,9 @@ gulp.task('build-prod', function(cb) {
 });
 
 gulp.task('build', function() {
-	environment = gutil.env.env || 'dev';
-
-	if(environment === 'prod') {
+	gutil.env.env = gutil.env.env || 'dev';
+console.log(gutil.env.env);
+	if(gutil.env.env === 'prod') {
 		runSequence( 
 			'build-prod'
 		);
@@ -248,62 +376,19 @@ gulp.task('build', function() {
 			'build-dev'
 		);
 	}
-})
-
-gulp.task('compileTemplates', function(){
-	return gulp.src('src/apps/*/views/*.html')
-		.pipe(handlebars({
-			handlebars: require('handlebars')
-		}))
-		.pipe(wrap('Handlebars.template(<%= contents %>)'))
-		.pipe(declare({
-			namespace: 'monster.cache.templates',
-			noRedeclare: true, // Avoid duplicate declarations ,
-			processName: function(filePath) {
-				// Allow nesting based on path using gulp-declare's processNameByPath()
-				// You can remove this option completely if you aren't using nested folders
-				// Drop the client/templates/ folder from the namespace path by removing it from the filePath
-				var splits = filePath.split('\\'),
-					// our files are all in folder such as apps/accounts/views/test.html, so we want to extract the last and 2 before last parts to have the app name and the template name
-					newName = splits[splits.length - 3] +'.' + splits[splits.length-1];
-				return declare.processNameByPath(newName);
-			}
-		}))
-		.pipe(concat('templates-compiled.js'))
-		.pipe(gulp.dest('tmp/js/'));
 });
 
-gulp.task('concatTemplates', function() {
-	return gulp.src(['src/js/templates.js', 'tmp/js/templates-compiled.js'])
-		.pipe(concat('templates.js'))
-		.pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('removeDistTemplates', function() {
-	return gulp.src('./dist/js/templates.js').pipe(clean());
-});
-
-gulp.task('templates', function(cb) {
-	runSequence('removeDistTemplates', 'compileTemplates', 'concatTemplates', 'clean-tmp', cb);
-})
-
-// watch our scss files so that when we change a variable it reloads the browser with the new css
-gulp.task('watch', function (){
-	runSequence('build', function() {
-		livereload.listen();
-
-		gulp.watch(_scss_path, ['sass']);
-	});
-});
-
-gulp.task('write-config', function() {
-	require('fs').writeFileSync('build-config.json', '{}');
-
-	return gulp.src('build-config.json')
-			.pipe(jeditor({
-				preloadedApps: getAppsToInclude()
-			}))
-			.pipe(gulp.dest('./dist'))
+gulp.task('build-app', function(cb) {
+	runSequence( 
+		'move-files', // moves all files but css to dist
+		'sass', // compiles all scss files into css and moves them to dist
+		'templates-app', // gets all the apps html templates and pre-compile them with handlebars, then append it to templates.js,
+		'require-app', // from dist, run the optimizer and output it into dist
+		'minify-js-app', // minifies js/main.js, we don't use the optimize from requirejs as we don't want to minify config.js
+		//'css', // takes all the apps provided up top and concatenate and minify them
+		//'write-config', // writes a config file for monster to know which apps have been minified so it doesn't reload the assets
+		cb
+	);
 });
 
 gulp.task('default', ['build']);
