0cab89ea36e408ec138a3a457aa2d0edc455a9a5
Joris Tirado <joris.tirado@gmail.com>
2016-10-19 10:47:44 -0700

    OOB: Add ordinal suffix as a pattern for toFriendlyDate() mask

--- src/js/lib/monster.util.js
+++ src/js/lib/monster.util.js
@@ -66,12 +66,32 @@ define(function(require){
 					hours = format2Digits(date.getHours()),
 					minutes = format2Digits(date.getMinutes()),
 					seconds = format2Digits(date.getSeconds()),
+					ordinal = (function oridnalSuffix(day) {
+						var i = day % 10,
+							j = day % 100;
+
+						// 'st' is used with numbers ending in 1 except 11
+						if (i === 1 && j !== 11) {
+							return 'st';
+						}
+						// 'nd' is used with numbers ending in 2 except 12
+						if (i === 2 && j !== 12) {
+							return 'nd';
+						}
+						// 'rd' is used with numbers ending in 3 except 13
+						if (i === 3 && j !== 13) {
+							return 'rd';
+						}
+						// 'th' is used for all other numbers
+						return 'th';
+					})(day),
 					shortDateFormats = {
 						'dmy': 'DD/MM/year',
 						'mdy': 'MM/DD/year',
 						'ymd': 'year/MM/DD'
 					},
 					patterns = {
+						'ordinal': ordinal,
 						'year': fullYear,
 						'YY': year,
 						'month': calendarMonth,
