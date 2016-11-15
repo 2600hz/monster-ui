7f7efa3146cd234c52c84ff055fde78e5068be8c
Joris Tirado <joris.tirado@gmail.com>
2016-11-15 14:09:51 -0800

    UI-2330: Refactor request handler for changing port request state

--- src/apps/common/submodules/port/port.js
+++ src/apps/common/submodules/port/port.js
@@ -316,8 +316,14 @@ define(function(require){
 					newState = el.val();
 
 				if (newState === 'rejected') {
-					self.portRequestChangeState(accountId, requestId, newState, function(newRequest) {
-						delete newRequest.scheduled_date;
+					self.portRequestChangeState({
+						data: {
+							accountId: accountId,
+							requestId: requestId,
+							state: newState
+						},
+						success: function(newRequest) {
+							delete newRequest.scheduled_date;
 
 						self.portRequestUpdate(accountId, requestId, newRequest, function(updatedRequest) {
 							self.portUpdateData(accountId, updatedRequest, data);
@@ -328,22 +334,31 @@ define(function(require){
 
 							toastr.success(self.i18n.active().port.toastr.success.request.update);
 						});
+						}
 					});
 				}
 				else if (newState === 'scheduled') {
 					self.portRenderScheduledDatePopup(parent, {
 						callbackSave: function (newScheduledDate) {
-							self.portRequestChangeState(accountId, requestId, newState, function(newRequest) {
-								newRequest.scheduled_date = newScheduledDate;
+							self.portRequestChangeState({
+								data: {
+									accountId: accountId,
+									requestId: requestId,
+									state: newState
+								},
+								success: function(newRequest) {
+									newRequest.scheduled_date = newScheduledDate;
 
-								self.portRequestUpdate(accountId, requestId, newRequest, function (updatedRequest) {
-									self.portUpdateData(accountId, updatedRequest, data);
-									self.portRenderDynamicCells(container, updatedRequest);
+									self.portRequestUpdate(accountId, requestId, newRequest, function (updatedRequest) {
+										self.portUpdateData(accountId, updatedRequest, data);
+										self.portRenderDynamicCells(container, updatedRequest);
 
-									toastr.success(self.i18n.active().port.toastr.success.request.update);
-								});
-							}, function() {
-								toastr.error(self.i18n.active().port.toastr.error.request.update);
+										toastr.success(self.i18n.active().port.toastr.success.request.update);
+									});
+								},
+								error: function() {
+									toastr.error(self.i18n.active().port.toastr.error.request.update);
+								}
 							});
 						},
 						callbackCancel: function() {
@@ -352,18 +367,27 @@ define(function(require){
 					});
 				}
 				else {
-					self.portRequestChangeState(accountId, requestId, newState, function(newRequest) {
-						self.portUpdateData(accountId, newRequest, data);
-						self.portRenderDynamicCells(container, newRequest);
+					self.portRequestChangeState({
+						data: {
+							accountId: accountId,
+							requestId: requestId,
+							state: newState
+						},
+						success: function(newRequest) {
+							self.portUpdateData(accountId, newRequest, data);
+							self.portRenderDynamicCells(container, newRequest);
 
-						box.find('.continue-request, .delete-request')
-							.remove();
+							box.find('.continue-request, .delete-request')
+								.remove();
 
-						toastr.success(self.i18n.active().port.toastr.success.request.update);
-					}, function() {
-						toastr.error(self.i18n.active().port.toastr.error.request.update);
-					}, function() {
-						self.portRenderDynamicCells(container, currentRequest);
+							toastr.success(self.i18n.active().port.toastr.success.request.update);
+						},
+						error: function() {
+							toastr.error(self.i18n.active().port.toastr.error.request.update);
+						},
+						cancel: function() {
+							self.portRenderDynamicCells(container, currentRequest);
+						}
 					});
 				}
 			});
@@ -1201,8 +1225,15 @@ define(function(require){
 								self.portReloadApp(parent, accountId);
 							}
 							else {
-								self.portRequestChangeState(accountId, order.id, 'submitted', function() {
-									self.portReloadApp(parent, accountId);
+								self.portRequestChangeState({
+									data: {
+										accountId: accountId,
+										requestId: order.id,
+										state: 'submitted'
+									},
+									success: function() {
+										self.portReloadApp(parent, accountId);
+									}
 								});
 							}
 						});
@@ -1211,16 +1242,23 @@ define(function(require){
 						self.portRequestAdd(accountId, order, function(portRequestId) {
 							data.orders.splice(index, 1);
 
-							self.portRequestChangeState(accountId, portRequestId, 'submitted', function() {
-								if (data.orders.length > 0) {
-									self.portRenderResumeOrders(parent, accountId, data);
+							self.portRequestChangeState({
+								data: {
+									accountId: accountId,
+									requestId: portRequestId,
+									state: 'submitted'
+								},
+								success: function() {
+									if (data.orders.length > 0) {
+										self.portRenderResumeOrders(parent, accountId, data);
+									}
+									else {
+										self.portReloadApp(parent, accountId);
+									}
+								},
+								cancel: function() {
+									self.portRequestDelete(accountId, portRequestId);
 								}
-								else {
-									self.portReloadApp(parent, accountId);
-								}
-							}, function() {
-							}, function() {
-								self.portRequestDelete(accountId, portRequestId);
 							});
 						});
 					}
@@ -2032,26 +2070,24 @@ define(function(require){
 			});
 		},
 
-		portRequestChangeState: function(accountId, portRequestId, state, callbackSuccess, callbackError, callbackCancel) {
+		portRequestChangeState: function(args) {
 			var self = this;
 
 			self.callApi({
 				resource: 'port.changeState',
-				data: {
-					accountId: accountId,
-					portRequestId: portRequestId,
-					state: state
-				},
+				data: _.extend({
+					accountId: self.accountId
+				}, args.data),
 				success: function(data, status) {
-					callbackSuccess && callbackSuccess(data.data);
+					args.hasOwnProperty('success') && args.success(data.data);
 				},
 				error: function(data, status) {
 					if (parseInt(data.error, 10) !== 402) {
-						callbackError && callbackError();
+						args.hasOwnProperty('error') && args.error();
 					}
 				},
-				onChargesCancelled: function () {
-					callbackCancel && callbackCancel();
+				onChargesCancelled: function() {
+					args.hasOwnProperty('cancel') && args.cancel();
 				}
 			});
 		},
