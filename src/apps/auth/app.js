277e8b03396c926a47c68299ce961f5fbbdbd68a
Joris Tirado <joris.tirado@gmail.com>
2017-01-17 15:42:30 -0800

    OOB: Fix error introduced by PR #67 (mmissing closing curly brace)

--- src/apps/auth/app.js
+++ src/apps/auth/app.js
@@ -111,13 +111,14 @@ define(function(require){
 				}, errorAuth);
 			}
 			// Otherwise, we check if some GET parameters are defined, and if they're formatted properly
-			
+
 			//APIkey generated tokens require UserId parameter to login.
 			else if(urlParams.hasOwnProperty('u') && urlParams.hasOwnProperty('t')) {
 				self.authenticateAuthToken(urlParams.t, function(authData) {
-					authData.data.owner_id = urlParams.u; 
+					authData.data.owner_id = urlParams.u;
 					successfulAuth(authData);
-			}, errorAuth);
+				}, errorAuth);
+			}
 			// Username/password generated tokens do not require anything else to log in.
 			else if(urlParams.hasOwnProperty('t')) {
 				self.authenticateAuthToken(urlParams.t, successfulAuth, errorAuth);
