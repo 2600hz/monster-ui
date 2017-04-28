eb5f5b8bdedc002558b177c889775f9be5f68225
Joris Tirado <joris@tirado.com>
2017-04-28 16:38:27 -0700

    UI-2494: Match libraries with NPM packages

--- src/js/main.js
+++ src/js/main.js
@@ -2,63 +2,157 @@
 require.config({
 	waitSeconds: 200,
 	paths: {
-		'async': 'js/vendor/async.min',
-		'bootstrap': 'js/vendor/bootstrap-2.3.1.min',
-		'bootstrap-clickover': 'js/vendor/bootstrapx-clickover-1.0',
-		'chart': 'js/vendor/chart.min',
-		'card': 'js/vendor/card',
-		'chosen': 'js/vendor/jquery.chosen.min',
-		'clipboard': 'js/vendor/clipboard.min',
-		'crossroads': 'js/vendor/crossroads.min',
 		'config': 'js/config',
-		'dependClass': 'js/vendor/jquery.dependClass',
-		'ddslick': 'js/vendor/jquery.ddslick.min',
-		'drop': 'js/vendor/drop',
-		'duo': 'js/vendor/duo.min',
-		'fileupload': 'js/lib/jquery.fileupload',
-		'footable': 'js/vendor/footable.min',
-		'form2object': 'js/vendor/form2object',
-		'handlebars': 'js/vendor/handlebars-v4.0.5',
-		'hasher': 'js/vendor/hasher.min',
-		'hotkeys': 'js/vendor/jquery.hotkeys.min',
-		'introJs': 'js/vendor/intro.min',
-		'isotope': 'js/vendor/jquery.isotope.min',
-		'jquery': 'js/vendor/jquery-1.9.1.min',
-		'jqueryui': 'js/vendor/jquery-ui-1.10.3.custom.min',
-		'jstz': 'js/vendor/jstz.min',
+
+		/**
+		 * custom libraries
+		 */
 		'kazoo': 'js/lib/kazoo/kazoo',
+		'fileupload': 'js/lib/jquery.fileupload',
 		'kazoosdk': 'js/lib/jquery.kazoosdk',
-		'libphonenumber': 'js/vendor/libphonenumber-js.min',
-		'mask': 'js/vendor/jquery.mask',
-		'modernizr': 'js/vendor/modernizr-2.6.2.min',
-		'monster': 'js/lib/monster',
 		'monster-apps': 'js/lib/monster.apps',
-		'monster-ui': 'js/lib/monster.ui',
-		'monster-ua': 'js/lib/monster.ua',
-		'monster-timezone': 'js/lib/monster.timezone',
+		'monster': 'js/lib/monster',
 		'monster-routing': 'js/lib/monster.routing',
 		'monster-socket': 'js/lib/monster.socket',
-		'monster-util': 'js/lib/monster.util',
+		'monster-timezone': 'js/lib/monster.timezone',
+		'monster-ua': 'js/lib/monster.ua',
+		'monster-ui': 'js/lib/monster.ui',
 		'monster-webphone': 'js/lib/monster.webphone',
-		'mousetrap': 'js/vendor/mousetrap-1.5.3.min',
-		'nicescroll': 'js/vendor/jquery.nicescroll.min',
+		'templates': 'js/templates',
+
+		/**
+		 * vendor libraries
+		 */
+
+		// same version: v2.1.4 (from source)
+		'async': 'node_modules/async/dist/async.min',
+
+		// same version: v2.3.1 (from source), installed as a tar
+		'bootstrap': 'node_modules/bootstrap/docs/assets/js/bootstrap.min',
+
+		// unused?
+		'bootstrap-clickover': 'js/vendor/bootstrapx-clickover-1.0',
+
+		// same version: v1.0.0-beta2 (assumed from commit history)
+		'card': 'node_modules/card/lib/js/card',
+
+		// v1.0.1-beta.2 (closest available) instead of v0.2.0 (assumed from commit history)
+		'chart': 'node_modules/chart.js/Chart.min',
+
+		// v1.5.1 (closest available) instead of v1.0.0 (from source)
+		'chosen': 'node_modules/chosen-js/chosen.jquery',
+
+		// same version: v1.5.15 (from source)
+		'clipboard': 'node_modules/clipboard/dist/clipboard',
+
+		// same version: v0.12.2 (from source)
+		'crossroads': 'node_modules/crossroads/dist/crossroads.min',
+
+		// can't find valid package
+		'dependClass': 'js/vendor/jquery.dependClass',
+
+		// v1.0.0 (unknown current)
+		'ddslick': 'node_modules/ddslick/dist/jquery.ddslick.min',
+
+		// same version: v1.4.1 (from source)
+		'drop': 'node_modules/tether-drop/dist/js/drop.min',
+
+		// same version: latest, no releases
+		'duo': 'node_modules/duo_web/js/Duo-Web-v1',
+
+		// not declared as a package
+		'footable': 'js/vendor/footable.min',
+
+		// v1.0.0 (unknown current)
+		'form2object': 'node_modules/form2js/src/form2js',
+
+		// same version: v4.0.5 (from source)
+		'handlebars': 'node_modules/handlebars/dist/handlebars',
+
+		// same version: v1.2.0 (from source)
+		'hasher': 'node_modules/hasher/dist/js/hasher',
+
+		// v0.2.2 (unknown current)
+		'hotkeys': 'node_modules/jquery-hotkeys/jquery-hotkeys',
+
+		// v1.1.0 instead of v1.0.0 (assumed from commit history)
+		'introJs': 'node_modules/intro.js/intro',
+
+		// v2.1.0 (closest available) instead of v1.5.25 (from source)
+		'isotope': 'node_modules/isotope-layout/js/isotope',
+
+		// same version: v1.9.1 (from source)
+		'jquery': 'node_modules/jquery/jquery',
+
+		// v1.10.4 (closest available) instead of v1.10.3 (from source)
+		'jqueryui': 'node_modules/jquery-ui/jquery-ui',
+
+		// can't find valid package
+		'jstz': 'js/vendor/jstz.min',
+
+		// latest packaged version not working
+		'libphonenumber': 'js/vendor/libphonenumber-js.min',
+
+		// same version: v1.13.9 (from source)
+		'mask': 'node_modules/jquery-mask-plugin/dist/jquery.mask',
+
+		// v3.0.0 (closest stable) instead of v2.6.2 (from source)
+		'modernizr': 'js/vendor/modernizr-2.6.2.min',
+
+		// need to build source
+		'monster-util': 'js/lib/monster.util',
+
+		// same version: v1.5.3 (from source)
+		'mousetrap': 'node_modules/mousetrap/mousetrap',
+
+		// v3.6.8 (closest available) instead of v3.2.0 (from source)
+		'nicescroll': 'node_modules/jquery.nicescroll/jquery.nicescroll',
+
+		//
 		'plugins': 'js/plugins',
-		'papaparse': 'js/vendor/papaparse-4.1.2.min',
-		'postal': 'js/vendor/postal-0.8.2',
-		'renderjson': 'js/vendor/renderjson',
-		'reqwest': 'js/vendor/reqwest-0.7.3.min',
-		'signals': 'js/vendor/signals.min',
+
+		// same version: v4.1.2 (from source)
+		'papaparse': 'node_modules/papaparse/papaparse',
+
+		// same version: v0.8.2 (from source)
+		'postal': 'node_modules/postal/lib/postal',
+
+		// same version: v1.2.0 (from source)
+		'renderjson': 'node_modules/renderjson/renderjson',
+
+		// same version: v0.7.3 (from source)
+		'reqwest': 'node_modules/reqwest/reqwest',
+
+		// same version: v1.0.0 (from source)
+		'signals': 'node_modules/signals/dist/signals',
+
+		// not a package
 		'slider': 'js/vendor/jquery.slider',
-		'tether': 'js/vendor/tether.min',
-		'timepicker': 'js/vendor/jquery.timepicker.min',
-		'toastr': 'js/vendor/toastr-1.3.0',
-		'touch-punch': 'js/vendor/jquery.ui.touch-punch.min',
-		'underscore': 'js/vendor/underscore.min',
-		'validate': 'js/vendor/jquery.validate.min',
+
+		// same version: v1.3.7 (from source)
+		'tether': 'node_modules/tether/dist/js/tether',
+
+		// v1.6.2 (closest available) instead of v1.1.8 (assumed from commit history)
+		'timepicker': 'node_modules/timepicker/jquery.timepicker',
+
+		// v2.0.3 (closest) instead of v1.3.0 (from source)
+		'toastr': 'node_modules/toastr/toastr',
+
+		// v0.2.3 (closest available) instead of v0.2.2 (from source)
+		'touch-punch': 'node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch',
+
+		// same version: v1.8.2 (from source)
+		'underscore': 'node_modules/underscore/underscore',
+
+		// same version: v1.11.1 (from source), installed as a tar
+		'validate': 'node_modules/jquery-validation/jquery.validate',
+
+		// not a package
 		'wysiwyg': 'js/vendor/bootstrap.wysiwyg.min',
-		'pdfjs-dist/build/pdf': 'js/vendor/pdfjs/build/pdf',
-		'pdfjs-dist/build/pdf.worker': 'js/vendor/pdfjs/build/pdf.worker',
-		'templates': 'js/templates'
+
+		// same version: v1.5.389 (azzumed from commit history)
+		'pdfjs-dist/build/pdf': 'node_modules/pdfjs-dist/build/pdf',
+		'pdfjs-dist/build/pdf.worker': 'node_modules/pdfjs-dist/build/pdf.worker'
 	},
 	shim: {
 		'card': ['jquery'],
