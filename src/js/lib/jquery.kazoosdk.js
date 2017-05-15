242efa8ffe20f57ad17e0ba44aa367fc8ca0e534
Jean-Roch Maitre <jr@2600hz.com>
2017-05-15 12:54:58 -0700

    UI-2733: adding httpErrorStatus to requests error payloads

--- src/js/lib/jquery.kazoosdk.js
+++ src/js/lib/jquery.kazoosdk.js
@@ -598,6 +598,7 @@
 
 			if('responseText' in error && error.responseText && error.getResponseHeader('content-type') === 'application/json') {
 				parsedError = $.parseJSON(error.responseText);
+				parsedError.httpErrorStatus = error.status;
 			}
 
 			// Added this to be able to display more data in the UI
