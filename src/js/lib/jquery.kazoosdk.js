250349d2881a2953bd497cdaf4760a98b84a931d
JR Maitre <jr@2600hz.com>
2017-08-16 08:46:37 -0700

    OOB: Updating security APIs

--- src/js/lib/jquery.kazoosdk.js
+++ src/js/lib/jquery.kazoosdk.js
@@ -342,11 +342,10 @@
 			},
 			security: {
 				'listModules': { verb: 'GET', url: 'security' },
-				'get': { verb: 'GET', url: 'accounts/{accountId}/security/{moduleId}' },
+				'get': { verb: 'GET', url: 'accounts/{accountId}/security' },
 				'create': { verb: 'PUT', url: 'accounts/{accountId}/security' },
-				'update': { verb: 'POST', url: 'accounts/{accountId}/security/{moduleId}' },
-				'delete': { verb: 'DELETE', url: 'accounts/{accountId}/security/{moduleId}' },
-				'list': { verb: 'GET', url: 'accounts/{accountId}/security' },
+				'update': { verb: 'POST', url: 'accounts/{accountId}/security' },
+				'delete': { verb: 'DELETE', url: 'accounts/{accountId}/security' },
 				'listAttempts': { verb: 'GET', url: 'accounts/{accountId}/security/attempts' },
 				'getAttempt': { verb: 'GET', url: 'accounts/{accountId}/security/attempts/{attemptId}' }
 			},
