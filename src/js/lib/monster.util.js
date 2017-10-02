9b380e40067a1300ee7a8eb665628d166b53a0dc
Joris Tirado <joris@tirado.com>
2017-10-02 16:14:46 -0700

    OOB: Fix conflict

--- src/js/lib/monster.util.js
+++ src/js/lib/monster.util.js
@@ -563,9 +563,6 @@ define(function(require) {
 			In last resort it will set it to 'en-US' if nothing is set above
 		*/
 		setDefaultLanguage: function() {
-<<<<<<< HEAD
-			var browserLanguage = (navigator.language).replace(/-.*/,function(a){return a.toUpperCase();}),// always capitalize the second part of the navigator language
-=======
 			var languages = [
 					'en-US',
 					'es-ES',
@@ -576,7 +573,6 @@ define(function(require) {
 				browserLanguage = _.find(languages, function(lang) {
 					return lang.indexOf(navigator.language) > -1;
 				}),
->>>>>>> 09dfa6b5... OOB: Use ISO formatted language of browser
 				cookieLanguage = $.cookie('monster-auth') ? ($.parseJSON($.cookie('monster-auth'))).language : undefined,
 				defaultLanguage = browserLanguage || 'en-US';
 
