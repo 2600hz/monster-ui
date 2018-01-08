1d92f3d20101999337496cf8d9c8c77cc54c564e
Jean-Roch Maitre <jr@2600hz.com>
2018-01-08 15:26:16 -0800

    UI-2989: first version of VERSION file creator
    Conflicts:
    	package.json

---
+++ gulp/tasks/write-version.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var paths = require('../paths');
+var package = require('../../package.json');
+
+gulp.task('write-version', function() {
+	var version = package.version,
+		fileName = paths.tmp + '/VERSION';
+
+	fs.writeFileSync(fileName, version);
+
+	return gulp.src(fileName);
+});
