9ea33202050e81971c9d3a0c56536ab9fb1cb7f5
James Aimonetti <james.aimonetti@gmail.com>
2018-04-27 23:22:18 +0000

    reconcile docs to index

---
+++ scripts/reconcile_docs_to_index.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pushd $(dirname $0) > /dev/null
+
+cd $(pwd -P)/..
+
+doc_count=0
+missing_count=0
+
+function check_index {
+    line=$(grep "$1" ./docs/mkdocs/mkdocs.yml)
+
+    if [ -f "$1" ] && [ -z "$line" ]; then
+        [[ 0 -eq $missing_count ]] && echo "Docs missing from the mkdocs.yml index:"
+        ((missing_count+=1))
+        echo "$missing_count: '$1'"
+    fi
+}
+
+docs=$(find docs \( -path 'doc/mkdocs' \) -prune -o -type f -regex ".+\.md$")
+for doc in $docs; do
+    ((doc_count+=1))
+    check_index $doc
+done
+
+if [[ 0 -lt $missing_count ]]; then
+    ratio=$((100 * $missing_count / $doc_count))
+    echo "Missing $missing_count / $doc_count: $ratio%"
+    popd > /dev/null
+    exit 1
+fi
+
+popd > /dev/null
