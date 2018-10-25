ba5653e784a740603007a462bd1c4df393b64d4f
Joris Tirado <joris@tirado.com>
2018-10-24 19:52:56 -0700

    UI-3200: Require new fields on port request creation (#217)
    * Add new address fields required by carriers
    
    * Require billing information fields required by carriers
    
    * Add and require new signee fields on port request creation

--- src/apps/common/submodules/portWizard/portWizard.js
+++ src/apps/common/submodules/portWizard/portWizard.js
@@ -1,6 +1,7 @@
 define(function(require) {
 	var $ = require('jquery'),
 		_ = require('lodash'),
+		moment = require('moment'),
 		monster = require('monster');
 
 	var portWizard = {
@@ -330,6 +331,7 @@ define(function(require) {
 				formType = self.portWizardGetFormType(request),
 				initTemplate = function initTemplate() {
 					var dataToTemplate = {
+							request: request,
 							type: self.i18n.active().portWizard.formTypes[formType],
 							formLink: monster.config.whitelabel.port[formType]
 						},
@@ -338,6 +340,10 @@ define(function(require) {
 							data: dataToTemplate,
 							submodule: 'portWizard'
 						})),
+						todayJsDate = moment().toDate(),
+						defaultJsDate = _.has(args.data.request, 'signing_date')
+							? monster.util.gregorianToDate(args.data.request.signing_date)
+							: todayJsDate,
 						actionsTemplate;
 
 					if (request.hasOwnProperty('uploads') && request.uploads.hasOwnProperty('form.pdf')) {
@@ -350,6 +356,10 @@ define(function(require) {
 							.find('.actions')
 								.append(actionsTemplate);
 					}
+
+					monster.ui.datepicker(template.find('#signing_date'), {
+						maxDate: todayJsDate
+					}).datepicker('setDate', defaultJsDate);
 
 					self.portWizardBindUploadFormEvents(template, args);
 
@@ -540,33 +550,51 @@ define(function(require) {
 			var self = this,
 				formValidationRules = {
 					'bill.name': {
+						required: true,
+						minlength: 1,
+						maxlength: 128
+					},
+					'bill.street_number': {
+						required: true,
+						digits: true,
+						minlength: 1,
+						maxlength: 8
+					},
+					'bill.street_type': {
+						required: true,
 						minlength: 1,
 						maxlength: 128
 					},
 					'bill.street_address': {
+						required: true,
 						minlength: 1,
 						maxlength: 128
 					},
 					'bill.locality': {
+						required: true,
 						minlength: 1,
 						maxlength: 128
 					},
 					'bill.region': {
+						required: true,
 						minlength: 2,
 						maxlength: 2
 					},
 					'bill.postal_code': {
+						required: true,
 						digits: true,
 						minlength: 5,
 						maxlength: 5
 					},
 					'bill.account_number': {
+						required: true,
 						maxlength: 128
 					},
 					'bill.pin': {
 						maxlength: 6
 					},
 					'bill.btn': {
+						required: true,
 						maxlength: 20
 					}
 				};
@@ -875,6 +903,7 @@ define(function(require) {
 
 		portWizardBindUploadFormEvents: function(template, args) {
 			var self = this,
+				$datepicker = template.find('#signing_date'),
 				fileUploadOptions = (function(data) {
 					var request = data.request,
 						options = {
@@ -975,7 +1004,28 @@ define(function(require) {
 				.on('click', '.uploadForm-success', function(event) {
 					event.preventDefault();
 
-					self.portWizardRenderPortNotify(args);
+					var formData = monster.ui.getFormData('form_upload_document'),
+						$form = template.find('#form_upload_document');
+
+					monster.ui.validate($form, {
+						rules: {
+							signee_name: {
+								required: true,
+								minlength: 1,
+								maxlength: 128
+							},
+							signing_date: {
+								required: true
+							}
+						}
+					});
+
+					if (monster.ui.valid($form)) {
+						$.extend(true, args.data.request, formData, {
+							signing_date: monster.util.dateToGregorian($datepicker.datepicker('getDate'))
+						});
+						self.portWizardRenderPortNotify(args);
+					}
 				});
 
 			template
