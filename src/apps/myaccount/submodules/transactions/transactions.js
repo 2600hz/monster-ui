203ce15134ae9caa087b299ef5bbe2b79ef8bb47
ricardo <masmerino@gmail.com>
2018-11-13 14:32:21 -0600

    Refactor transactions module

--- src/apps/myaccount/submodules/transactions/transactions.js
+++ src/apps/myaccount/submodules/transactions/transactions.js
@@ -22,47 +22,45 @@ define(function(require) {
 					to: moment().toDate()
 				};
 
-			self.listTransactions(defaultRange.from, defaultRange.to, function(data) {
-				var transactionsView = $(self.getTemplate({
-						name: 'layout',
-						data: data,
-						submodule: 'transactions'
-					})),
-					listTransactionsView = $(self.getTemplate({
-						name: 'list',
-						data: data,
-						submodule: 'transactions'
-					})),
-					optionsDatePicker = {
-						container: transactionsView,
-						range: self.transactionsRange,
-						startDate: defaultRange.from
-					};
+			self.listTransactions({
+				from: defaultRange.from,
+				to: defaultRange.to,
+				callback: function(data) {
+					var transactionsView = $(self.getTemplate({
+							name: 'layout',
+							data: {
+								billingStartDate: data.billingStartDate,
+								billingEndDate: data.billingEndDate,
+								amount: data.amount
+							},
+							submodule: 'transactions'
+						})),
+						listTransactionsView = $(self.getTemplate({
+							name: 'list',
+							data: {
+								listTransactions: data.listTransactions
+							},
+							submodule: 'transactions'
+						})),
+						optionsDatePicker = {
+							container: transactionsView,
+							range: self.transactionsRange,
+							startDate: defaultRange.from
+						};
 
-				transactionsView.find('.list-transactions').append(listTransactionsView);
+					transactionsView.find('.list-transactions').append(listTransactionsView);
 
-				monster.ui.initRangeDatepicker(optionsDatePicker);
+					monster.ui.initRangeDatepicker(optionsDatePicker);
 
-				self.transactionsBindEvents(transactionsView);
+					self.transactionsBindEvents(transactionsView);
 
-				monster.pub('myaccount.renderSubmodule', transactionsView);
+					monster.pub('myaccount.renderSubmodule', transactionsView);
 
-				if (typeof args.callback === 'function') {
-					args.callback(transactionsView);
+					if (typeof args.callback === 'function') {
+						args.callback(transactionsView);
+					}
 				}
 			});
-		},
-
-		transactionsFormatData: function(data) {
-			var self = this;
-
-			data.amount = parseFloat(data.amount).toFixed(2);
-
-			data.listTransactions.sort(function(a, b) {
-				return a.created < b.created ? 1 : -1;
-			});
-
-			return data;
 		},
 
 		transactionsBindEvents: function(parent, data) {
@@ -80,103 +78,73 @@ define(function(require) {
 			});
 
 			parent.find('#filter_transactions').on('click', function() {
-				var from = monster.util.parseDateString(parent.find('#startDate').val()),
-					to = monster.util.parseDateString(parent.find('#endDate').val());
+				self.listTransactions({
+					from: parent.find('#startDate').datepicker('getDate'),
+					to: parent.find('#endDate').datepicker('getDate'),
+					callback: function(data) {
+						var listTransactions = parent.find('.list-transactions').empty();
 
-				self.listTransactions(from, to, function(data) {
-					var listTransactions = parent.find('.list-transactions').empty();
+						listTransactions
+							.append($(self.getTemplate({
+								name: 'list',
+								data: {
+									listTransactions: data.listTransactions
+								},
+								submodule: 'transactions'
+							})));
 
-					listTransactions
-						.append($(self.getTemplate({
-							name: 'list',
-							data: data,
-							submodule: 'transactions'
-						})));
+						parent.find('.expandable').hide();
 
-					parent.find('.expandable').hide();
-
-					parent.find('.start-date .value').html(data.billingStartDate);
-					parent.find('.end-date .value').html(data.billingEndDate);
-					parent.find('.total-amount .value').html(monster.util.formatPrice({
-						price: data.amount
-					}));
+						parent.find('.start-date .value').html(data.billingStartDate);
+						parent.find('.end-date .value').html(data.billingEndDate);
+						parent.find('.total-amount .value').html(monster.util.formatPrice({
+							price: data.amount
+						}));
+					}
 				});
 			});
 		},
 
 		// from, to: optional together. Date objects.
-		listTransactions: function(from, to, callback) {
+		listTransactions: function(args) {
 			var self = this;
-
-			if (typeof from === 'function') {
-				callback = from;
-
-				var dates = monster.util.getDefaultRangeDates(self.balanceRange);
-
-				from = dates.from;
-				to = dates.to;
-			}
 
 			var defaults = {
 				amount: 0.00,
-				billingStartDate: moment(from).format(monster.util.getMomentFormat('date')),
-				billingEndDate: moment(to).format(monster.util.getMomentFormat('date'))
+				billingStartDate: moment(args.from).format(monster.util.getMomentFormat('date')),
+				billingEndDate: moment(args.to).format(monster.util.getMomentFormat('date'))
 			};
-
-			// We search from the beginning of the from date, to the end of the to date
-			from = monster.util.dateToBeginningOfGregorianDay(from);
-			to = monster.util.dateToEndOfGregorianDay(to);
-
-			monster.parallel({
-				charges: function(callback) {
-					self.transactionsGetCharges(from, to, function(dataCharges) {
-						var arrayCharges = [];
-
-						$.each(dataCharges.data, function(k, v) {
-							if (v.status && v.status == 'pending') {
-                                                                  return true;
-                                                        }
-
-                					v = monster.util.formatTransaction(v, self);
-							arrayCharges.push(v);
-
-							if (v.approved) {
-							        defaults.amount += parseFloat(v.amount);
-							}
-						});
-
-						callback(null, arrayCharges);
-					});
-				}
-			}, function(err, results) {
-				var renderData = defaults;
-
-				renderData.listTransactions = results.charges;
-
-				renderData = self.transactionsFormatData(renderData);
-
-				callback(renderData);
-			});
-		},
-
-		transactionsGetCharges: function(from, to, success, error) {
-			var self = this;
 
 			self.callApi({
 				resource: 'transactions.list',
 				data: {
 					accountId: self.accountId,
 					filters: {
-						created_from: from,
-						created_to: to,
+						created_from: monster.util.dateToBeginningOfGregorianDay(args.from),
+						created_to: monster.util.dateToEndOfGregorianDay(args.to),
 						reason: 'no_calls'
 					}
 				},
-				success: function(data, status) {
-					success && success(data, status);
-				},
-				error: function(data, status) {
-					error && error(data, status);
+				success: function(transactions) {
+					defaults.listTransactions = _.chain(transactions.data)
+						.filter(function(transaction) {
+							return _.get(transaction, 'status', 'pending') !== 'pending';
+						})
+						.map(function(transaction) {
+							return monster.util.formatTransaction(transaction, self);
+						})
+						.sortBy('created', 'asc')
+						.value();
+
+					defaults.amount = _.reduce(defaults.listTransactions, function(total, trans) {
+						if (trans.approved) {
+							return total + parseFloat(trans.amount);
+						}
+					}, 0);
+
+					defaults.amount = parseFloat(defaults.amount).toFixed(2);
+
+					args.callback(defaults);
 				}
 			});
 		}
