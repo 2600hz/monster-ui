263c1770fb447a7adaf1b4290ed45cdc162bafc4
Pilar Candia <pilar@2600hz.com>
2018-12-04 15:02:03 -0500

    Add check marks to navigation menu

--- src/apps/common/submodules/navigationWizard/navigationWizard.js
+++ src/apps/common/submodules/navigationWizard/navigationWizard.js
@@ -14,12 +14,16 @@ define(function(require) {
 
 		navigationWizardRender: function(args) {
 			var self = this,
+				thisArg = args.thisArg,
 				container = args.container,
+				steps = args.steps,
+				currentStep = 0,
 				layout = $(self.getTemplate({
 					name: 'layout',
 					data: {
 						title: args.title,
-						steps: args.steps
+						steps: args.steps,
+						doneButton: args.doneButton ? args.doneButton : ''
 					},
 					submodule: 'navigationWizard'
 				}));
@@ -30,6 +34,9 @@ define(function(require) {
 				container
 					.empty()
 					.append(layout);
+
+				self.generateTemplate(currentStep, args);
+
 			} else {
 				throw new Error('A container must be provided.');
 			}
@@ -38,6 +45,7 @@ define(function(require) {
 		navigationWizardBindEvents: function(args) {
 			var self = this,
 				container = args.container,
+				thisArg = args.thisArg,
 				template = args.template,
 				steps = args.steps,
 				removeCallback = args.removeCallback,
@@ -45,23 +53,49 @@ define(function(require) {
 				currentStep = 0;
 
 			self.setSelected(currentStep, args);
+			self.generateTemplate(currentStep, args);
 
-			//Clicking on the menu item
+			//Clicking next on the menu item
 			template
-				.find('.next')
+				.find('#next')
 					.on('click', function(event) {
 						event.preventDefault();
-						console.log(steps[currentStep].form);
 
-				/*		var $form = template.find('#'+steps[currentStep].form),
-							formData = monster.ui.getFormData(steps[currentStep].form);
+						var result = self.utilForTemplate(currentStep, args);
 
-						console.log($form);
-						console.log(formData);*/
-						currentStep += 1;
-						self.setSelected(currentStep, args);
+						if (result.valid === true) {
+							//display checkbox
+							template
+								.find('i[data-id="' + currentStep + '"]')
+								.removeClass('hide-check');
 
+							template
+								.find('li[data-id="' + currentStep + '"]')
+								.addClass('done');
+
+							//merge data
+							args = _.merge({}, args, {
+								data: result.data
+							});
+
+							currentStep += 1;
+							self.setSelected(currentStep, args);
+							self.generateTemplate(currentStep, args);
+						}
 					});
+
+			//Clicking next on the menu item
+			template
+				.find('#done')
+					.on('click', function(event) {
+						event.preventDefault();
+
+						var result = self.utilForTemplate(currentStep, args);
+
+						if (result.valid === true) {
+							thisArg[args.done](args);				
+						}
+			});
 
 			//Clicking on the menu item
 			template
@@ -71,6 +105,7 @@ define(function(require) {
 
 						currentStep -= 1;
 						self.setSelected(currentStep, args);
+						self.generateTemplate(currentStep, args);
 					});
 
 			//Clicking on cancel
@@ -79,16 +114,16 @@ define(function(require) {
 					.on('click', function(event) {
 						event.preventDefault();
 
-						container
-							.empty()
-							.append(args.cancel);
+						thisArg[args.cancel](args);
 					});
 
 			template
 				.find('#clear')
 				.on('click', function(event) {
 					event.preventDefault();
-					console.log('clear');
+
+					//it still needs work
+					console.log('clear');					
 				});
 
 		},
@@ -98,30 +133,62 @@ define(function(require) {
 				template = args.template,
 				steps = args.steps;
 			
-					template
-						.find('.right-content')
-						.empty()
-						.append(steps[currentStep].template);
+				template
+					.find('.right-content')
+					.empty()
+					.append(steps[currentStep].template);
 
-					monster.ui.tooltips(template);
+				template
+					.find('li')
+					.removeClass('selected');
 
-					template
-						.find('li')
-						.removeClass('selected');
+				template
+					.find('.nav span')
+					.addClass('hide-content')
+					.removeClass('show-content');
 
-					template
-						.find('.nav span')
-						.addClass('hide-content')
-						.removeClass('show-content');
+				template
+					.find('span[data-id="' + currentStep + '"]')
+					.addClass('show-content')
+					.removeClass('hide-content');
 
-					template
-						.find('span[data-id="' + currentStep + '"]')
-						.addClass('show-content')
-						.removeClass('hide-content');
+				template
+					.find('li[data-id="' + currentStep + '"]')
+					.addClass('selected');
 
-					template
-						.find('li[data-id="' + currentStep + '"]')
-						.addClass('selected');
+			//Hide back button in the first page
+			if (currentStep === 0) {
+				template.find('.back').hide();
+			} else {
+				template.find('.back').show();
+			}
+
+			//Display done button
+			if ((steps.length - 1) === currentStep) {
+				template.find('#next').hide();
+				template.find('#done').show();
+			} else {
+				template.find('#done').hide();
+				template.find('#next').show();
+			}
+		},
+
+		generateTemplate: function(currentStep, args) {
+			var self = this,
+				thisArg = args.thisArg,
+				steps = args.steps,
+				template = steps[currentStep].template;
+
+			thisArg[template](args);
+		},
+		
+		utilForTemplate: function(currentStep, args) {
+			var self = this,
+				thisArg = args.thisArg,
+				steps = args.steps,
+				util = steps[currentStep].util;
+
+			return thisArg[util](args.template, args);
 		}
 	};
 
