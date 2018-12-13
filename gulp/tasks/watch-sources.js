a79ba3cf04f19a4d22627c79c4897e337cd56a68
Guillermo Gutierrez <guillegr123@gmail.com>
2018-12-13 16:25:25 -0600

    Create common function to build pipeline for watch

--- gulp/tasks/watch-sources.js
+++ gulp/tasks/watch-sources.js
@@ -1,3 +1,4 @@
+import browserSync from 'browser-sync';
 import del from 'del';
 import gulp from 'gulp';
 import cache from 'gulp-cached';
@@ -5,53 +6,89 @@ import sass from 'gulp-sass';
 import path from 'path';
 import { src, dist } from '../paths.js';
 
+/**
+ * Build gulp pipeline for watch events
+ * @param  {Object}                          args
+ * @param  {browserSync.BrowserSyncInstance} args.server            BrowserSync server instance
+ * @param  {String}                          args.fileExt           Input file extension
+ * @param  {Boolean}                         [args.useCache=false]  Use cache
+ * @param  {Boolean}                         [args.reload=false]    Reload server
+ * @param  {NodeJS.ReadWriteStream}          [args.process]         Additional process step on pipeline
+ */
+const buildWatchPipeline = args => {
+	let fileExt = args.fileExt,
+		process = args.process,
+		useCache = !!args.useCache,
+		reload = !!args.reload,
+		server = args.server,
+		pipeline = gulp.src(`${src}/**/*.${fileExt}`);
+
+	if (process) {
+		pipeline = pipeline.pipe(process);
+	}
+	if (useCache) {
+		pipeline = pipeline.pipe(cache(fileExt));
+	}
+
+	pipeline = pipeline.pipe(gulp.dest(dist))
+
+	if (reload) {
+		return pipeline.pipe(server.reload({stream:true}));
+	}
+
+	return pipeline.pipe(server.stream());
+}
+
 const getWatchSassFn = server =>
 	// Return a named function, to allow Gulp to display its name
 	function watchSass() {
-		return gulp
-			.src(src + '/**/*.scss')
-			.pipe(sass().on('error', sass.logError))
-			.pipe(gulp.dest(dist))
-			.pipe(server.stream());
+		return buildWatchPipeline({
+			server,
+			fileExt: 'scss',
+			process: sass().on('error', sass.logError)
+		});
 	};
 
 const getWatchCssFn = server =>
 	// Return a named function, to allow Gulp to display its name
 	function watchCss() {
-		return gulp
-			.src(src + '/**/*.css')
-			.pipe(gulp.dest(dist))
-			.pipe(server.stream());
+		return buildWatchPipeline({
+			server,
+			fileExt: 'css'
+		});
 	};
 
 const getWatchJsFn = server =>
 	// Return a named function, to allow Gulp to display its name
 	function watchJs() {
-		return gulp
-			.src(src + '/**/*.js')
-			.pipe(cache('js'))
-			.pipe(gulp.dest(dist))
-			.pipe(server.reload({stream:true}));
+		return buildWatchPipeline({
+			server,
+			fileExt: 'js',
+			useCache: true,
+			reload: true
+		});
 	};
 
 const getWatchHtmlFn = server =>
 	// Return a named function, to allow Gulp to display its name
 	function watchHtml() {
-		return gulp
-			.src(src + '/**/*.html')
-			.pipe(cache('html'))
-			.pipe(gulp.dest(dist))
-			.pipe(server.reload({stream:true}));
+		return buildWatchPipeline({
+			server,
+			fileExt: 'html',
+			useCache: true,
+			reload: true
+		});
 	};
 
 const getWatchJsonFn = server =>
 	// Return a named function, to allow Gulp to display its name
 	function watchJson() {
-		return gulp
-			.src(src + '/**/*.json')
-			.pipe(cache('json'))
-			.pipe(gulp.dest(dist))
-			.pipe(server.reload({stream:true}));
+		return buildWatchPipeline({
+			server,
+			fileExt: 'json',
+			useCache: true,
+			reload: true
+		});
 	};
 
 const getUnlinkFileFn = server =>
