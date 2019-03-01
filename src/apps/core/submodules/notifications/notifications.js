d91de726024033d7eb4ba4382de4d518177b24e1
Guillermo Gutierrez <guillegr123@gmail.com>
2019-03-01 16:00:33 -0600

    Add main functions and empty layout to submodule

--- src/apps/core/submodules/notifications/notifications.js
+++ src/apps/core/submodules/notifications/notifications.js
@@ -9,8 +9,95 @@ define(function(require) {
 
 		// Define the events available for other apps
 		subscribe: {
-			'core.alerts.hideDropdown': 'TODO',
-			'core.alerts.refresh': 'TODO'
+			'core.alerts.hideDropdown': 'notificationsHideDropdown',
+			'core.alerts.refresh': 'notificationsRender'
+		},
+
+		/**
+		 * Fetch alerts from API, resets component to default state (hide dropdown and show notifications count if any)
+		 * @param  {Object} args
+		 */
+		notificationsRender: function(args) {
+			var self = this,
+				container = args.container,
+				initTemplate = function initTemplate(notificationsData) {
+					var formattedNotifications = self.notificationsFormatData({
+							data: notificationsData
+						}),
+						dataTemplate = {
+							notifications: formattedNotifications
+						},
+						$template = $(self.getTemplate({
+							name: 'layout',
+							data: dataTemplate,
+							submodule: 'trunks'
+						}));
+
+					self.notificationsBindEvents({
+						template: $template
+					});
+				};
+
+			monster.waterfall([
+				function(callback) {
+					self.notificationsHideDropdown();
+
+					self.notificationsRequestListAlerts({
+						success: function(data) {
+							callback(null, data);
+						},
+						error: function(parsedError) {
+							callback(parsedError);
+						}
+					});
+				}
+			], function(err, data) {
+				if (err) {
+					_.has(args, 'callbacks.error') && args.callbacks.error(err);
+				} else {
+					initTemplate(data);
+
+					_.has(args, 'callbacks.success') && args.callbacks.success();
+				}
+			});
+		},
+
+		/**
+		 * Hides the notifications dropdown at the DOM
+		 */
+		notificationsHideDropdown: function() {
+			// TODO
+		},
+
+		notificationsFormatData: function(args) {
+
+		},
+
+		/**
+		 * Bind template content events
+		 * @param  {Object} args
+		 * @param  {jQuery} args.template    Template to bind
+		 */
+		notificationsBindEvents: function(args) {
+			var self = this,
+				template = args.template;
+		},
+
+		notificationsRequestListAlerts: function(args) {
+			var self = this;
+
+			self.callApi({
+				resource: 'alert.list',
+				data: {
+					accountId: self.accountId
+				},
+				success: function(data, status) {
+					_.has(args, 'success') && args.success(data.data, status);
+				},
+				error: function(data, status) {
+					_.has(args, 'error') && args.error(data, status);
+				}
+			});
 		}
 	};
 
