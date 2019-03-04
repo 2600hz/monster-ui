71e87e4c1d9ec60cb7a2464afde1abad6dab3b14
Guillermo Gutierrez <guillegr123@gmail.com>
2019-03-04 09:35:58 -0600

    Remove functions and events not related to ticket

--- src/apps/core/submodules/notifications/notifications.js
+++ src/apps/core/submodules/notifications/notifications.js
@@ -7,85 +7,6 @@ define(function(require) {
 		// Defines API requests not included in the SDK
 		requests: {},
 
-		// Define the events available for other apps
-		subscribe: {
-			'core.alerts.hideDropdown': 'notificationsHideDropdown',
-			'core.alerts.refresh': 'notificationsRender'
-		},
-
-		/**
-		 * Fetch alerts from API into the notifications dropdown, and resets component to default
-		 * state (hide dropdown and show notifications count if any)
-		 * @param  {Object}   args
-		 * @param  {jQUery}   args.container            Dropdown container
-		 * @param  {Object}   [args.callbacks]          Optional callbacks
-		 * @param  {Function} [args.callbacks.success]  Success callback
-		 * @param  {Function} [args.callbacks.error]    Success callback
-		 */
-		notificationsRender: function(args) {
-			var self = this,
-				$container = args.container,
-				initTemplate = function initTemplate(notificationsData) {
-					var formattedNotifications = self.notificationsFormatData({
-							data: notificationsData
-						}),
-						dataTemplate = {
-							notifications: formattedNotifications
-						},
-						$template = $(self.getTemplate({
-							name: 'layout',
-							data: dataTemplate,
-							submodule: 'trunks'
-						}));
-
-					self.notificationsBindEvents({
-						template: $template
-					});
-				};
-
-			monster.waterfall([
-				function(callback) {
-					self.notificationsHideDropdown();
-
-					if ($container) {
-						callback();
-					} else {
-						callback(ReferenceError('The "container" arg is required to load the account browser.'));
-					}
-				},
-				function(callback) {
-					self.notificationsRequestListAlerts({
-						success: function(data) {
-							callback(null, data);
-						},
-						error: function(parsedError) {
-							callback(parsedError);
-						}
-					});
-				},
-				function(data, callback) {
-					$container
-						.empty()
-						.append(initTemplate(data));
-
-					callback(null);
-				}
-			], function(err) {
-				if (err) {
-					_.has(args, 'callbacks.error') && args.callbacks.error(err);
-				} else {
-					_.has(args, 'callbacks.success') && args.callbacks.success();
-				}
-			});
-		},
-
-		/**
-		 * Hides the notifications dropdown at the DOM
-		 */
-		notificationsHideDropdown: function() {
-			// TODO
-		},
-
 		/**
 		 * Formats the notification data received from the API, into UI categories
 		 * @param    {Object}   args
@@ -108,18 +29,6 @@ define(function(require) {
 
 				return notificationType;
 			}).value();
-		},
-
-		/**
-		 * Bind template content events
-		 * @param  {Object} args
-		 * @param  {jQuery} args.template    Template to bind
-		 */
-		notificationsBindEvents: function(args) {
-			var self = this,
-				$template = args.template;
-
-			// TODO
 		},
 
 		/**
