b76eaa7c7e992d55d20f8c6e6de0b6ba41ccd47c
Guillermo Gutiérrez <guillegr123@gmail.com>
2019-03-05 08:34:34 -0600

    UI-3318: Create submodule to fetch /alerts of current account (#286)

---
+++ src/apps/core/submodules/alerts/alerts.js
@@ -0,0 +1,59 @@
+define(function(require) {
+	var $ = require('jquery'),
+		_ = require('lodash'),
+		monster = require('monster');
+
+	var alerts = {
+		// Defines API requests not included in the SDK
+		requests: {},
+
+		/**
+		 * Formats the alert data received from the API, into UI categories
+		 * @param    {Object}   args
+		 * @param    {Object[]} args.data  Array of alerts
+		 * @returns  {Object}              Grouped alerts by UI categories
+		 */
+		alertsFormatData: function(args) {
+			var self = this;
+
+			return _.groupBy(args.data, function(alert) {
+				var alertType;
+
+				if (alert.clearable) {
+					alertType = 'manual';
+				} else if (_.includes([ 'low_balance', 'no_payment_token', 'expired_payment_token' ], alert.category)) {
+					alertType = 'system';
+				} else {
+					alertType = 'apps';
+				}
+
+				return alertType;
+			}).value();
+		},
+
+		/**
+		 * Request alerts list from API
+		 * @param  {Object}   args
+		 * @param  {Function} [args.success]  Success callback
+		 * @param  {Function} [args.error]    Error callback
+		 */
+		alertsRequestListAlerts: function(args) {
+			var self = this;
+
+			self.callApi({
+				resource: 'alert.list',
+				data: {
+					accountId: monster.apps.auth.currentAccount.id
+				},
+				success: function(data, status) {
+					_.has(args, 'success') && args.success(data.data, status);
+				},
+				error: function(parsedError) {
+					_.has(args, 'error') && args.error(parsedError);
+				}
+			});
+		}
+	};
+
+	return alerts;
+});
