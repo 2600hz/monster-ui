c0eb28a07c117548a6fef6d2d0e95613258b6f2b
Guillermo Gutierrez <guillegr123@gmail.com>
2019-03-06 15:57:05 -0600

    Add clear button

--- src/apps/core/submodules/alerts/alerts.js
+++ src/apps/core/submodules/alerts/alerts.js
@@ -181,7 +181,8 @@ define(function(require) {
 						title: alertData.title,
 						metadata: metadata,
 						message: alertData.message,
-						category: category
+						category: category,
+						clearable: alertData.clearable
 					};
 				})
 				.groupBy(function(alert) {
@@ -199,7 +200,6 @@ define(function(require) {
 						alertType = category.substring(0, dashIndex > 0 ? dashIndex : category.length);
 						alert.iconPath = monster.util.getAppIconPath({ name: alertType });
 					}
-					console.log(_.merge({}, { alertType: alertType }, alert));
 
 					return alertType;
 				}).map(function(alerts, type) {
