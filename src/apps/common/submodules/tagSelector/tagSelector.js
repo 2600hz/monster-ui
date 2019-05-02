5f36bfe81ccac75f911a64716d160aef36e0cfe2
ricardo <masmerino@gmail.com>
2019-05-02 12:48:17 -0600

    MSTR-1: Make add tag dialog workable

--- src/apps/common/submodules/tagSelector/tagSelector.js
+++ src/apps/common/submodules/tagSelector/tagSelector.js
@@ -27,10 +27,13 @@ define(function(require) {
 					submodule: 'tagSelector'
 				})),
 				optionsPopup = {
-					position: ['center', 20],
-					title: title || self.i18n.active().tagSelector.title
+					position: ['center', 100],
+					title: title || self.i18n.active().tagSelector.title,
+					width: 400
 				},
 				popup = monster.ui.dialog(template, optionsPopup);
+
+			template.find('.tags-search .tags-keyword').focus();
 
 			self.tagSelectorBindEvents(_.merge({}, args, {
 				template: template,
@@ -39,7 +42,123 @@ define(function(require) {
 		},
 
 		tagSelectorBindEvents: function(args) {
-			console.log('binding', args);
+			var self = this,
+				template = args.template,
+				popup = args.popup,
+				callback = args.callback;
+
+			template
+				.find('#cancel')
+					.on('click', function(event) {
+						popup.dialog('close').remove();
+					});
+
+			template
+				.find('.tags-search .tags-keyword')
+					.on('keyup', function(event) {
+						event.preventDefault();
+
+						var $this = $(this),
+							searchString = $this.val().toLowerCase(),
+							items = $this.parents('.add-tags-dialog-content-wrapper').find('.tags-list a'),
+							stringArray = searchString.split(' '),
+							arrayLength = stringArray.length,
+							textToFind = stringArray.length > 0 ? stringArray[arrayLength - 1] : searchString;
+
+						if (textToFind.length > 0) {
+							template
+								.find('.tags-list')
+									.removeClass('hide')
+									.addClass('show');
+						} else {
+							template
+								.find('.tags-list')
+									.removeClass('show')
+									.addClass('hide');
+						}
+
+						_.each(items, function(item) {
+							var $item = $(item),
+								value = $item.html().toLowerCase();
+
+							value.indexOf(textToFind) < 0 || $item.hasClass('selected') ? $item.hide() : $item.show();
+						});
+					});
+
+			template
+				.find('.tags-list a')
+					.on('click', function(event) {
+						event.preventDefault();
+
+						var $this = $(this),
+							tag = $this.html(),
+							id = $this.data('id'),
+							newDiv = $(self.getTemplate({
+								name: 'tag',
+								data: {
+									tag: tag,
+									id: id
+								},
+								submodule: 'tagSelector'
+							}));
+
+						template
+							.find('.tags-search .tags')
+								.prepend(newDiv);
+
+						template
+							.find('.tags-search .tags-keyword')
+								.attr('placeholder', '')
+								.val('')
+								.focus();
+
+						template
+							.find('.tags-list')
+								.toggleClass('hide show');
+
+						$this.addClass('selected');
+					});
+
+			template
+				.on('click', '.tags-search svg', function(event) {
+					event.preventDefault();
+
+					var $this = $(this),
+						$tag = $this.parents('.tag'),
+						tagsCount = template.find('.tag').length;
+
+					$tag.remove();
+
+					if (tagsCount === 0) {
+						template
+							.find('.tags-search .tags-keyword')
+								.attr('placeholder', self.i18n.active().tagSelector.searchTags);
+					}
+
+					template
+						.find('.tags-list a[data-id="' + $tag.data('id') + '"]')
+							.removeClass('selected');
+					template
+						.find('.tags-search .tags-keyword')
+							.focus();
+				});
+
+			template
+				.find('.add-tags')
+					.on('click', function(event) {
+						var tags = _.map(template.find('.tags-search .tag'), function(tag) {
+							var $tag = $(tag);
+
+							return {
+								id: $tag.data('id'),
+								tag: $tag.data('name')
+							};
+						});
+
+						callback && callback(tags);
+
+						popup.dialog('close').remove();
+					});
 		}
 	};
 
