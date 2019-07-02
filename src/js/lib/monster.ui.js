2b763a45bdd776b6521d09a93829b88da9372c05
Guillermo Gutierrez <guillegr123@gmail.com>
2019-07-02 16:14:27 -0600

    Extract existing default validation handlers
    `$.validator.defaults.errorPlacement` was removed because it does not
    exist

--- src/js/lib/monster.ui.js
+++ src/js/lib/monster.ui.js
@@ -1372,40 +1372,42 @@ define(function(require) {
 		},
 
 		validate: function(form, options) {
-			var defaultOptions = {
-				errorClass: 'monster-invalid',
-				validClass: 'monster-valid',
-				errorPlacement: function(error, element) {
-					var $element = $(element);
-					if ($element.hasClass('ui-spinner-input')) {
-						error.insertAfter($element.closest('.ui-spinner'));
-					} else {
-						$.validator.defaults.errorPlacement.call(this, error, element);
+			var defaultValidatorHighlightHandler = $.validator.defaults.highlight,
+				defaultValidatorUnhighlightHandler = $.validator.defaults.unhighlight,
+				defaultOptions = {
+					errorClass: 'monster-invalid',
+					validClass: 'monster-valid',
+					errorPlacement: function(error, element) {
+						var $element = $(element);
+						if ($element.hasClass('ui-spinner-input')) {
+							error.insertAfter($element.closest('.ui-spinner'));
+						} else {
+							error.insertAfter(element);
+						}
+					},
+					highlight: function(element, errorClass, validClass) {
+						var $element = $(element);
+						if ($element.hasClass('ui-spinner-input')) {
+							$element
+								.closest('.ui-spinner')
+									.addClass(errorClass)
+									.removeClass(validClass);
+						} else {
+							defaultValidatorHighlightHandler.call(this, element, errorClass, validClass);
+						}
+					},
+					unhighlight: function(element, errorClass, validClass) {
+						var $element = $(element);
+						if ($element.hasClass('ui-spinner-input')) {
+							$element
+								.closest('.ui-spinner')
+									.addClass(validClass)
+									.removeClass(errorClass);
+						} else {
+							defaultValidatorUnhighlightHandler.call(this, element, errorClass, validClass);
+						}
 					}
-				},
-				highlight: function(element, errorClass, validClass) {
-					var $element = $(element);
-					if ($element.hasClass('ui-spinner-input')) {
-						$element
-							.closest('.ui-spinner')
-								.addClass(errorClass)
-								.removeClass(validClass);
-					} else {
-						$.validator.defaults.highlight.call(this, element, errorClass, validClass);
-					}
-				},
-				unhighlight: function(element, errorClass, validClass) {
-					var $element = $(element);
-					if ($element.hasClass('ui-spinner-input')) {
-						$element
-							.closest('.ui-spinner')
-								.addClass(validClass)
-								.removeClass(errorClass);
-					} else {
-						$.validator.defaults.unhighlight.call(this, element, errorClass, validClass);
-					}
-				}
-			};
+				};
 
 			if (!this.customValidationInitialized) {
 				this.initCustomValidation();
