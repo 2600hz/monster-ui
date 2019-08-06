227726e1d1b4fe34e9ddb8881be10c513c12b9e7
Joris Tirado <joris@tirado.com>
2019-08-06 13:33:40 -0700

    Plug 401 handler into sdk custom error handler

--- src/js/lib/monster.js
+++ src/js/lib/monster.js
@@ -597,50 +597,11 @@ define(function(require) {
 							requestOptions.onChargesCancelled && requestOptions.onChargesCancelled();
 						});
 					} else if (monster.util.isLoggedIn() && error.status === 401) {
-						// If we have a 401 after being logged in, it means our session expired, or that it's a MFA denial of the relogin attempt
-						// We don't want to show the normal error box for 401s, but still want to check the payload if they happen, via the error tool.
-						monster.error('api', error, false);
-
-						// the prevent callback error key is added by our code. We want to let the system automatically attempt to relogin once before sending the error callback
-						// So to prevent the original error callback from being fired, we add that flag to the request when we attempt the request a second time
-						if (!requestOptions.hasOwnProperty('preventCallbackError') || requestOptions.preventCallbackError === false) {
-							// If it's a retryLoginRequest, we don't want to prevent the callback as it could be a MFA denial
-							if (!requestOptions.hasOwnProperty('isRetryLoginRequest') || requestOptions.isRetryLoginRequest === false) {
-								if (!_privateFlags.lockRetryAttempt) {
-									// We added a new locking mechanism. Basically if your module use a parallel request, you could have 5 requests ending in a 401. We don't want to automatically re-login 5 times, so we lock the system
-									// Once the re-login is effective, we'll unlock it.
-									_privateFlags.lockRetryAttempt = true;
-
-									// Because onRequestError is executed before error in the JS SDK, we can set this flag to prevent the execution of the custom error callback
-									// This way we can handle the 401 properly, try again with a new auth token, and continue the normal flow of the ui
-									requestOptions.preventCallbackError = true;
-
-									monster.apps.auth.retryLogin({ isRetryLoginRequest: true }, function(newToken) {
-										// We setup the flag to false this time, so that if it errors out again, we properly log out of the UI
-										var updatedRequestOptions = $.extend(true, requestOptions, { preventCallbackError: false, authToken: newToken });
-
-										monster.kazooSdk.request(updatedRequestOptions);
-
-										_privateFlags.unlockRetryFunctions();
-									},
-									function() {
-										monster.ui.alert('error', monster.apps.core.i18n.active().invalidCredentialsMessage, function() {
-											monster.util.logoutAndReload();
-										});
-									});
-								} else {
-									_privateFlags.addRetryFunction(function() {
-										var updatedRequestOptions = $.extend(true, requestOptions, { authToken: monster.util.getAuthToken() });
-
-										monster.kazooSdk.request(updatedRequestOptions);
-									});
-								}
-							}
-						} else {
-							monster.ui.alert('error', monster.apps.core.i18n.active().invalidCredentialsMessage, function() {
-								monster.util.logoutAndReload();
-							});
-						}
+						error401Handler({
+							requestHandler: monster.kazooSdk.request,
+							error: error,
+							options: requestOptions
+						});
 					} else {
 						monster.error('api', error, requestOptions.generateError);
 					}
