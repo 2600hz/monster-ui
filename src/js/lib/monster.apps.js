cc37eeaea40abde95058412d413f9fad38e6264c
Joris Tirado <joris@tirado.com>
2019-08-16 16:43:32 -0700

    Wait for async operations before invoking callback
    Loading dependencies is an asynchonous operation since a HTTP request
    needs to be made to retrieve the file in question.
    Since loading an extension is equivalent to loading an app, it could
    potentiallly trigger dependency loading as well, hence making the
    operation asynchronous as well.
    
    This problem is solved by simply bundling all dependencies/extensions
    loading operations in an array of tasks executed in parallel.
    
    Inifite extensions loading is taken care of by simply checking if an
    extension (which is an app) is already loaded locally.

--- src/js/lib/monster.apps.js
+++ src/js/lib/monster.apps.js
@@ -348,33 +348,47 @@ define(function() {
 			});
 		},
 
+		/**
+		 * @param  {Object} app
+		 * @param  {Function} [globalCallback]
+		 */
 		loadDependencies: function(app, globalCallback) {
 			var self = this,
-				listRequests = {},
-				externalPath = app.appPath + '/external/',
-				deps = app.externalScripts || [];
+				dependencies = _
+					.chain(app)
+					.get('externalScripts', [])
+					.map(function(dependency) {
+						return function(callback) {
+							monster.getScript(app.appPath + '/external/' + dependency + '.js', function() {
+								callback(null);
+							});
+						};
+					})
+					.value(),
+				extensions = _
+					.chain(monster.appsStore)
+					.get([app.name, 'extensions'], [])
+					.map(function(extension) {
+						return function(callback) {
+							if (_.has(monster.apps, extension)) {
+								return callback(null);
+							}
+							self._loadApp(extension, function() {
+								callback(null);
+							});
+						};
+					})
+					.value();
 
-			if (monster.appsStore && monster.appsStore[app.name] && monster.appsStore[app.name].pluginsApps) {
-				_.each(monster.appsStore[app.name].pluginsApps, function(plugin) {
-					self._loadApp(plugin);
-				});
-			}
-
-			if (deps.length > 0) {
-				_.each(deps, function(name) {
-					listRequests[name] = function(callback) {
-						monster.getScript(externalPath + name + '.js', function() {
-							callback(null, {});
-						});
-					};
-				});
-
-				monster.parallel(listRequests, function(err, results) {
+			monster.parallel(
+				_.concat(
+					dependencies,
+					extensions
+				),
+				function(err, results) {
 					globalCallback && globalCallback();
-				});
-			} else {
-				globalCallback && globalCallback();
-			}
+				}
+			);
 		},
 
 		_addAppCss: function(app) {
