3ffc3624b7d593b172601bd215da981e6aaeb885
ricardo <masmerino@gmail.com>
2019-12-09 11:41:14 -0600

    Create simplemde function

--- src/js/lib/monster.ui.js
+++ src/js/lib/monster.ui.js
@@ -15,7 +15,8 @@ define(function(require) {
 		mousetrap = require('mousetrap'),
 		Drop = require('drop'),
 		Clipboard = require('clipboard'),
-		moment = require('moment');
+		moment = require('moment'),
+		simplemde = require('simplemde');
 
 	require('chosen');
 	require('disableAutoFill');
@@ -1463,6 +1464,28 @@ define(function(require) {
 
 		valid: function(form) {
 			return form.valid();
+		},
+
+		/**
+		 * @desc Convert the 'target' in to Markdown editor
+		 * @param target - mandatory jQuery object
+		 * @param options - optional object to overwrite the default configs
+		 */
+		simplemde: function($target, options) {
+			if (!($target instanceof $)) {
+				throw TypeError('"$target" is not a jQuery object');
+			}
+
+			if (!_.isUndefined(options) && !_.isPlainObject(options)) {
+				throw TypeError('"options" is not a plain object');
+			}
+
+			new simplemde(_.merge({
+				element: $target[0],
+				status: false,
+				autosave: false,
+				spellChecker: false
+			}, options));
 		},
 
 		/**
