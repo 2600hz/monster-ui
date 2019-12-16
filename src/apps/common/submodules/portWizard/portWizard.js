54f8c0f6e0b1ce6a0a95faed387e769e317935fc
Guillermo Gutierrez <guillegr123@gmail.com>
2019-12-16 09:32:11 -0600

    Improve number filtering logic and visual effects

--- src/apps/common/submodules/portWizard/portWizard.js
+++ src/apps/common/submodules/portWizard/portWizard.js
@@ -527,12 +527,14 @@ define(function(require) {
 						submodule: 'portWizard'
 					}));
 
+					monster.ui.mask($template.find('.search-query'), 'phoneNumber');
+
 					self.portWizardCarrierSelectionSingleBindEvents({
 						formattedNumbers: formattedNumbers,
 						template: $template
 					});
 
-					// TODO: Event binding and form validation
+					// TODO: Form validation
 
 					return $template;
 				};
@@ -673,19 +675,17 @@ define(function(require) {
 		portWizardCarrierSelectionSingleBindEvents: function(args) {
 			var self = this,
 				formattedNumbers = args.formattedNumbers,
+				numbersShown = _.map(formattedNumbers, 'e164Number'),
 				$template = args.template,
-				$numberList = $template.find('.number-list');
-
-			$template
-				.find('input.search-query')
-					.on('keyup', _.debounce(function(e) {
-						var value = _.trim($(this).val()),
-							$elementsToShow = $(),
-							$elementsToHide = $();
-
-						_.each(formattedNumbers, function(formattedNumberData) {
-							var $numberItem = $numberList.find('[data-e164="' + formattedNumberData.e164Number + '"]'),
-								shouldDisplayNumberItem = _
+				$elements = $template.find('.number-list .number-item'),
+				$elementsShown = $elements,
+				filterNumberElements = function(filterText) {
+					var $elementsToShow = $(),
+						$elementsToHide = $elementsShown,
+						numbersToShow = _
+							.chain(formattedNumbers)
+							.filter(function(formattedNumberData) {
+								return _
 									.chain(formattedNumberData)
 									.pick([
 										'e164Number',
@@ -694,20 +694,57 @@ define(function(require) {
 										'userFormat'
 									])
 									.some(function(formattedNumber) {
-										return _.includes(formattedNumber, value);
+										return _.includes(formattedNumber, filterText);
 									})
 									.value();
+							})
+							.map('e164Number')
+							.value(),
+						hiddenElementsCount = 0;
 
-							if (shouldDisplayNumberItem) {
-								$elementsToShow = $elementsToShow.add($numberItem);
-							} else {
-								$elementsToHide = $elementsToHide.add($numberItem);
+					if (_.isEqual(numbersShown, numbersToShow)) {
+						return;
+					}
+
+					_.each(numbersToShow, function(number) {
+						var $numberItem = $elements.filter('[data-e164="' + number + '"]');
+						$elementsToShow = $elementsToShow.add($numberItem);
+					});
+
+					numbersShown = numbersToShow;
+					$elementsShown = $elementsToShow;
+
+					monster.waterfall([
+						function(waterfallCallback) {
+							if ($elementsToHide.length === 0) {
+								return waterfallCallback(null);
 							}
-						});
+							$elementsToHide.fadeOut(150, function() {
+								// The complete callback function is called once per element
+								hiddenElementsCount += 1;
+								if (hiddenElementsCount < $elementsToHide.length) {
+									return;
+								}
 
-						$elementsToShow.removeClass('number-item-hidden');
-						$elementsToHide.addClass('number-item-hidden');
-					}));
+								waterfallCallback(null);
+							});
+						}
+					], function() {
+						if ($elementsToShow.length === 0) {
+							return;
+						}
+
+						$elementsToShow.fadeIn(150);
+					});
+				};
+
+			$template
+				.find('input.search-query')
+					.on('keyup', _.debounce(function() {
+						var value = _.trim($(this).val());
+
+						filterNumberElements(value);
+					}, 350));
 		},
 
 		/**
