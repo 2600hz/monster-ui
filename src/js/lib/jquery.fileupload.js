a8f1e40d94642dd163e18d62743e9f79f502f3a7
Guillermo Gutierrez <guillegr123@gmail.com>
2019-12-27 10:18:48 -0600

    Make file upload companion text input clickable

--- src/js/lib/jquery.fileupload.js
+++ src/js/lib/jquery.fileupload.js
@@ -6,7 +6,8 @@
 			inputOnly: false,
 			multiple: false,
 			resultType: '[]',
-			dataFormat: 'dataURL'
+			dataFormat: 'dataURL',
+			enableInputClick: false
 		};
 
 	$.fn.fileUpload = function(args) {
@@ -31,6 +32,13 @@
 			}
 
 			wrapper.insertAfter(fileInput).append(fileInput, input, button);
+
+			if (options.enableInputClick) {
+				input
+					.on('click', function() {
+						fileInput.trigger('click');
+					});
+			}
 		} else {
 			wrapper.insertAfter(fileInput).append(fileInput, input, button);
 			bigButton.insertBefore(wrapper);
@@ -120,9 +128,9 @@
 						var color = input.css('color'),
 							bgColor = input.css('backgroundColor');
 
-						input.animate({'color': bgColor}, 300, function() {
+						input.animate({ 'color': bgColor }, 300, function() {
 							input.val(names);
-							input.animate({'color': color}, 300);
+							input.animate({ 'color': color }, 300);
 						});
 					} else {
 						input.val(names);
@@ -146,6 +154,7 @@
 		});
 
 		wrapper.delegate('#file_upload_input_'.concat(id), 'keydown', function(event) {
+			console.log('event.which', event.which);
 			switch (event.which) {
 				case 8:
 				case 46:
