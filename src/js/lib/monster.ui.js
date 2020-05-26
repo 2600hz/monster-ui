b4241b235c730ccc0b7cb6f8808fa74cbbf1b1c9
Fernando González <fernandomenjivar2011@gmail.com>
2020-05-26 14:26:03 -0600

    Create helper function for jsoneditor

--- src/js/lib/monster.ui.js
+++ src/js/lib/monster.ui.js
@@ -18,6 +18,7 @@ define(function(require) {
 		moment = require('moment'),
 		simplemde = require('simplemde'),
 		marked = require('marked');
+		JSONEditor = require('jsoneditor');
 
 	require('chosen');
 	require('disableAutoFill');
@@ -1523,6 +1524,37 @@ define(function(require) {
 		 */
 		markdownToHtml: function(content) {
 			return marked(content);
+		},
+
+		/**
+		 * Create a new instance of jsoneditor
+		 * @param {jQuery} $target Contatiner to append the editor to
+		 * @param {Object} [options] Editor options
+		 * @param {Object} [json] JSON object to set in the editor as initial data
+		 * @param {String} [customClass] Custom ccs class for the container
+		 * @return {JSONEditor} editor
+		 */
+		jsoneditor: function($target, options, json, customClass) {
+			if (!($target instanceof $)) {
+				throw TypeError('"$target" is not a jQuery object');
+			}
+
+			$target
+				.addClass(customClass ? customClass : 'monster-jsoneditor');
+
+			var container = $target[0],
+				jsonObject = json || {},
+				formattedOptions = _.merge({}, {
+					mode: 'code',
+					enableSort: false,
+					enableTransform: false,
+					search: true
+				}, options),
+				editor = new JSONEditor(container, formattedOptions);
+
+			editor.set(jsonObject);
+
+			return editor;
 		},
 
 		/**
