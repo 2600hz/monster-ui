7aaa8e3290ab9718cb7fe5546bed07f5bab85f4e
Pilar Candia <pilar@2600hz.com>
2020-09-29 14:40:06 -0400

    First commit for hotjar to support dynamic includes for hotjar and other third party script

---
+++ src/js/vendor/hotjar.js
@@ -0,0 +1,10 @@
+	(function(h,o,t,j,a,r){
+		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		//monster.config.whitelabel.hotjarID is configured in config.js
+		//h._hjSettings={hjid:1995636,hjsv:6};
+		h._hjSettings={hjid:monster.config.whitelabel.hotjarID,hjsv:6};
+		a=o.getElementsByTagName('head')[0];
+		r=o.createElement('script');r.async=1;
+		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		a.appendChild(r);
+	})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
