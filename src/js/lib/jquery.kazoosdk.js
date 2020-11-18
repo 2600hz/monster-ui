4f6baf71ebaebf0d6fc1951bfef3f211bb7fa5d0
masmerino <masmerino@gmail.com>
2020-11-18 13:10:27 -0600

    Keep new definitions alphabetically

--- src/js/lib/jquery.kazoosdk.js
+++ src/js/lib/jquery.kazoosdk.js
@@ -94,6 +94,10 @@
 				'listByInteraction': { verb: 'GET', url: 'accounts/{accountId}/cdrs/interaction' },
 				'listLegs': { verb: 'GET', url: 'accounts/{accountId}/cdrs/legs/{callId}' }
 			},
+			cdrPushOnebill: {
+				'get': { verb: 'GET', url: 'accounts/{accountId}/cdr_push_onebill' },
+				'post': { verb: 'POST', url: 'accounts/{accountId}/cdr_push_onebill' }
+			},
 			channel: {
 				'list': { verb: 'GET', url: 'accounts/{accountId}/channels' },
 				'action': { verb: 'PUT', url: 'accounts/{accountId}/channels/{callId}', removeMetadataAPI: true }
@@ -558,10 +562,6 @@
 				'deleteNotification': { verb: 'DELETE', url: 'accounts/{accountId}/notifications/{notificationId}' },
 				'getDnsEntries': { verb: 'GET', url: 'accounts/{accountId}/whitelabel/domains' },
 				'checkDnsEntries': { verb: 'POST', url: 'accounts/{accountId}/whitelabel/domains?domain={domain}' }
-			},
-			onebillIntegration: {
-				'get': { verb: 'GET', url: 'accounts/{accountId}/cdr_push_onebill' },
-				'post': { verb: 'POST', url: 'accounts/{accountId}/cdr_push_onebill' }
 			}
 		},
 		authTokens = {};
