9b758e7dc4ba5c7ecf17b5beb69d86252199c4e2
Pilar Candia <pilar@2600hz.com>
2024-02-02 11:24:37 -0500

    MSTR-281: Fix issue where payload changed for API "phone_numbers" API (#1131)
    * MSTR-281: Fix issue where 'numbers.get' payload changed
    
    * Add changes suggested in code review

--- src/apps/common/submodules/numberRenameCarrier/numberRenameCarrier.js
+++ src/apps/common/submodules/numberRenameCarrier/numberRenameCarrier.js
@@ -40,10 +40,10 @@ define(function(require) {
 					carriers: carriers
 				};
 
-			if (dataNumber.hasOwnProperty('_read_only') && dataNumber._read_only.hasOwnProperty('carrier_module')) {
+			if (dataNumber.hasOwnProperty('metadata') && dataNumber.metadata.hasOwnProperty('carrier_module')) {
 				_.each(carriers, function(carrier) {
-					if (dataNumber._read_only.carrier_module === carrier.key) {
-						formattedData.selectedCarrier = dataNumber._read_only.carrier_module;
+					if (dataNumber.metadata.carrier_module === carrier.key) {
+						formattedData.selectedCarrier = dataNumber.metadata.carrier_module;
 					}
 				});
 
