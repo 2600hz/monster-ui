f81a2347917eb2a564f6021a167f2deaa42c0deb
Pilar Candia <pilar@2600hz.com>
2024-03-15 20:55:21 -0400

    testing

--- src/apps/myaccount/submodules/billing/billing.js
+++ src/apps/myaccount/submodules/billing/billing.js
@@ -40,88 +40,21 @@ define(function(require) {
 							callback(null, {});
 						}
 					});
-				},
-				accountToken: function(callback) {
-					self.callApi({
-						resource: 'billing.getToken',
-						data: {
-							accountId: self.accountId
-						},
-						success: function(data, status) {
-							callback(null, data.data);
-						}
-					});
 				}
 			}, function(err, results) {
 				self.billingFormatData(results, function(results) {
 					var billingTemplate = $(self.getTemplate({
-							name: 'layout',
-							data: results,
-							submodule: 'billing'
-						})),
-						setCardHeader = function(creditCard, button) {
-							var cardType = _.get(creditCard, 'card_type', '').toLowerCase(),
-								newTypeClass = cardType === 'american express'
-									? 'card-type amex'
-									: 'card-type ' + cardType,
-								newDescription = creditCard.last_four
-									? '•••• •••• •••• ' + (creditCard.last_four)
-									: '';
-
-							billingTemplate.find('.card-type')
-								.removeClass()
-								.addClass(newTypeClass);
-
-							billingTemplate.find('.fake-number')
-								.text(newDescription);
-
-							if (!_.isEmpty(creditCard)) {
-								button.removeClass('show');
-							} else {
-								button.addClass('show');
-							}
-						};
-
-					self.billingBindEvents(billingTemplate, results);
+						name: 'layout',
+						data: results,
+						submodule: 'billing'
+					}));
 
 					monster.pub('myaccount.renderSubmodule', billingTemplate);
 
-					dropin.create({
-						authorization: _.get(results, 'accountToken.client_token'),
-						selector: '#dropin_container',
-						vaultManager: true,
-						card: {
-							cardholderName: {
-								required: true
-							}
-						}
-					}, function(err, instance) {
-						var saveButton = billingTemplate.find('.save-card'),
-							deleteButton = billingTemplate.find('.braintree-delete-confirmation__button[data-braintree-id="delete-confirmation__yes"]');
+					self.renderCreditCardSection(billingTemplate);
+					self.renderAddButtonsSection(billingTemplate);
 
-						saveButton.on('click', function() {
-							instance.requestPaymentMethod(function(err, payload) {
-								if (err) {
-									instance.clearSelectedPaymentMethod();
-								} else {
-									self.requestUpdateBilling({
-										data: {
-											data: {
-												nonce: payload.nonce
-											}
-										},
-										success: function(data) {
-											setCardHeader(_.head(_.get(data, 'credit_cards')), saveButton);
-										}
-									});
-								}
-							});
-						});
-
-						deleteButton.on('click', function() {
-							setCardHeader({}, saveButton);
-						});
-					});
+					self.billingBindEvents(billingTemplate, results);
 
 					if (typeof args.callback === 'function') {
 						args.callback(billingTemplate);
@@ -150,11 +83,136 @@ define(function(require) {
 			callback(data);
 		},
 
+		renderCreditCardSection: function(template) {
+			var self = this;
+
+			monster.parallel({
+				accountToken: function(callback) {
+					self.callApi({
+						resource: 'billing.getToken',
+						data: {
+							accountId: self.accountId,
+							generateError: false
+						},
+						success: function(data, status) {
+							callback(null, data.data);
+						},
+						error: function(data, status) {
+							callback(null, {});
+						}
+					});
+				}
+			}, function(err, results) {
+				console.log(results);
+				var setCardHeader = function(creditCard, button) {
+					var cardType = _.get(creditCard, 'card_type', '').toLowerCase(),
+						newTypeClass = cardType === 'american express'
+							? 'card-type amex'
+							: 'card-type ' + cardType,
+						newDescription = creditCard.last_four
+							? '•••• •••• •••• ' + (creditCard.last_four)
+							: '';
+
+					template.find('.card-type')
+						.removeClass()
+						.addClass(newTypeClass);
+
+					template.find('.fake-number')
+						.text(newDescription);
+
+					if (!_.isEmpty(creditCard)) {
+						button.removeClass('show');
+					} else {
+						button.addClass('show');
+					}
+				},
+				clientToken = _.get(results, 'accountToken.client_token');
+
+				console.log(clientToken);
+				dropin.create({
+					authorization: clientToken || 'sandbox_g42y39zw_348pk9cgf3bgyw2b', //default client token to display the credit card section
+					selector: '#dropin_container',
+					vaultManager: true,
+					card: {
+						cardholderName: {
+							required: true
+						}
+					}
+				}, function(err, instance) {
+					var saveButton = template.find('.save-card'),
+						deleteButton = template.find('.braintree-delete-confirmation__button[data-braintree-id="delete-confirmation__yes"]');
+
+					console.log(clientToken);
+					template.on('click', '.save-card', function(event) {
+						console.log(clientToken);
+						event.preventDefault();
+						if (!clientToken) {
+							console.log('here');
+							self.requestUpdateBilling({
+								data: {
+									data: {
+										first_name: 'Test',
+										last_name: 'Test'
+									}
+								},
+								success: function(data) {
+									self.renderCreditCardSection(template);
+									self.renderAddButtonsSection(template);
+								}
+							});
+							console.log('stop');
+							return;
+						}
+
+						instance.requestPaymentMethod(function(err, payload) {
+							if (err) {
+								instance.clearSelectedPaymentMethod();
+							} else {
+								self.requestUpdateBilling({
+									data: {
+										data: {
+											nonce: payload.nonce
+										}
+									},
+									success: function(data) {
+										console.log(data);	
+										setCardHeader(_.head(_.get(data, 'credit_cards')), saveButton);
+									}
+								});
+							}
+						});
+					});
+
+					deleteButton.on('click', function() {
+						setCardHeader({}, saveButton);
+					});
+				});
+			});
+		},
+
+		renderAddButtonsSection: function(mainTemplate) {
+			var self = this,
+				appendTemplate = function appendTemplate() {
+					var template = $(self.getTemplate({
+						name: 'credit-card-buttons',
+						submodule: 'billing'
+					}));
+
+					mainTemplate
+						.find('.credit-card-buttons')
+						.replaceWith(template);
+				};
+
+			appendTemplate();
+		},
+
 		billingBindEvents: function(template, data) {
 			var self = this,
 				creditCardData = _.get(data, 'billing.credit_card');
 
+			console.log(creditCardData);
 			if (_.isEmpty(creditCardData)) {
+				console.log('inside here');
 				template.find('.save-card').addClass('show');
 			}
 
@@ -192,6 +250,22 @@ define(function(require) {
 				data: _.merge({
 					accountId: self.accountId
 				}, args.data),
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
+		},
+
+		getAccountToken: function(callback) {
+			self.callApi({
+				resource: 'billing.getToken',
+				data: {
+					accountId: self.accountId,
+					generateError: false
+				},
 				success: function(data) {
 					args.hasOwnProperty('success') && args.success(data.data);
 				},
