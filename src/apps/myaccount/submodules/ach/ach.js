5c9b36a02f78e3632798ea28efeb8cc5b6855874
Pilar Candia <pilar@2600hz.com>
2024-05-10 11:24:46 -0400

    [5.3] MSTR-291: Implement ACH section with different bank states (#1169)
    * MSTR-291: Implement APIs for the ACH
    
    * Update language files
    
    * Store bank account in the vault
    
    * Add status badge to the ACH Direct Debit card
    
    * Add verified bank account view
    
    * Fix language files and fix linter

--- src/apps/myaccount/submodules/ach/ach.js
+++ src/apps/myaccount/submodules/ach/ach.js
@@ -2,7 +2,6 @@ define(function(require) {
 	var $ = require('jquery'),
 		_ = require('lodash'),
 		monster = require('monster'),
-		client = require('braintree-client'),
 		usBankAccount = require('braintree-us-bank-account');
 
 	var ach = {
@@ -24,29 +23,77 @@ define(function(require) {
 
 		subscribe: {},
 
+		requests: {
+			'myaccount.getAccount': {
+				url: 'accounts/{accountId}',
+				verb: 'GET'
+			},
+			'myaccount.braintree.putAchToken': {
+				apiRoot: monster.config.whitelabel.achApi,
+				url: '/accounts/{accountId}/braintree/ach/vault',
+				verb: 'PUT',
+				generateError: false,
+				removeHeaders: [
+					'X-Kazoo-Cluster-ID'
+				]
+			},
+			'myaccount.braintree.deleteAchMethod': {
+				apiRoot: monster.config.whitelabel.achApi,
+				url: '/accounts/{accountId}/braintree/ach/{paymentMethodToken}',
+				verb: 'DELETE',
+				generateError: false,
+				removeHeaders: [
+					'X-Kazoo-Cluster-ID'
+				]
+			},
+			'myaccount.braintree.confirmMicroDeposits': {
+				apiRoot: monster.config.whitelabel.achApi,
+				url: '/accounts/{accountId}/braintree/ach/{verificationId}/confirm',
+				verb: 'PUT',
+				generateError: false,
+				removeHeaders: [
+					'X-Kazoo-Cluster-ID'
+				]
+			},
+			'myaccount.braintree.getVerificationStatus': {
+				apiRoot: monster.config.whitelabel.achApi,
+				url: '/accounts/{accountId}/braintree/ach/{verificationId}',
+				verb: 'GET',
+				generateError: false,
+				removeHeaders: [
+					'X-Kazoo-Cluster-ID'
+				]
+			},
+			'myaccount.braintree.getAch': {
+				apiRoot: monster.config.whitelabel.achApi,
+				url: '/accounts/{accountId}/braintree/ach',
+				verb: 'GET',
+				generateError: false,
+				removeHeaders: [
+					'X-Kazoo-Cluster-ID'
+				]
+			}
+		},
+
 		achRenderSection: function(args) {
 			var self = this,
 				container = args.container,
-				data = args.data,
 				appendTemplate = function appendTemplate() {
 					var template = $(self.getTemplate({
 							name: 'ach-section',
 							submodule: 'ach'
 						})),
-						beginVerificationButton = template.find('.begin-verification'),
-						enableFormButton = function() {
-							if (_.every(self.appFlags.ach.validAchFormFields)) {
-								beginVerificationButton.removeClass('disabled');
-							} else {
-								beginVerificationButton.addClass('disabled');
-							}
+						$beginVerificationButton = template.find('.begin-verification'),
+						enableBeginVerificationButton = function() {
+							var isFormValid = _.every(self.appFlags.ach.validAchFormFields);
+							$beginVerificationButton.prop('disabled', !isFormValid);
 						},
 						$achForm = template.find('#form_ach_payment'),
 						billingContactData = self.appFlags.billing.billingContactFields,
 						firstname = billingContactData['contact.billing.first_name'].value,
 						lastname = billingContactData['contact.billing.last_name'].value;
 
-					enableFormButton();
+					enableBeginVerificationButton();
 
 					//Set agreement name
 					template.find('.agreement-name').text(firstname + ' ' + lastname);
@@ -78,7 +125,7 @@ define(function(require) {
 							template.find('.agreement-' + name).text(value);
 							self.appFlags.ach.validAchFormFields[name] = isValid;
 
-							enableFormButton();
+							enableBeginVerificationButton();
 						},
 						autoScrollOnInvalid: true
 					});
@@ -94,8 +141,30 @@ define(function(require) {
 							}, function(usBankAccountErr, usBankAccountInstance) {
 								next(usBankAccountErr, clientInstance, usBankAccountInstance);
 							});
+						},
+						function braintreeAch(usBankAccountErr, usBankAccountInstance, next) {
+							self.getBraintreeAch({
+								success: function(bankData) {
+									next(null, usBankAccountErr, usBankAccountInstance, bankData);
+								}
+							});
+						},
+						function braintreeAchStatus(usBankAccountErr, usBankAccountInstance, bankData, next) {
+							if (_.isEmpty(bankData)) {
+								next(null, usBankAccountErr, usBankAccountInstance, bankData, []);
+							} else {
+								var newBankData = _.head(bankData);
+								self.getVerificationStatus({
+									data: {
+										verificationId: _.get(newBankData, 'verification_id')
+									},
+									success: function(statusData) {
+										next(null, usBankAccountErr, usBankAccountInstance, newBankData, statusData);
+									}
+								});
+							}
 						}
-					], function(usBankAccountErr, clientInstance, usBankAccountInstance) {
+					], function(usBankAccountErr, clientInstance, usBankAccountInstance, bankData, statusData) {
 						monster.pub('monster.requestEnd', {});
 
 						if (usBankAccountErr && _.get(usBankAccountErr, 'code') === 'US_BANK_ACCOUNT_NOT_ENABLED') {
@@ -112,12 +181,30 @@ define(function(require) {
 							return;
 						}
 
-						beginVerificationButton.on('click', function(event) {
+						if (!_.isEmpty(bankData) && _.get(statusData, 'status') === 'pending') {
+							self.achRenderVerificationSection(_.merge({}, args, {
+								data: {
+									bankData: bankData
+								}
+							}));
+
+							return;
+						}
+
+						if (!_.isEmpty(bankData) && _.get(statusData, 'status') === 'verified') {
+							self.achRenderShowBank(_.merge({}, args, {
+								data: {
+									bankData: bankData
+								}
+							}));
+
+							return;
+						}
+
+						$beginVerificationButton.on('click', function(event) {
 							event.preventDefault();
 
-							if (beginVerificationButton.hasClass('disabled')) {
-								return;
-							}
+							$beginVerificationButton.prop('disabled', true);
 							var achDebitData = monster.ui.getFormData('form_ach_payment'),
 								mandateText = template.find('.agreement1').text() + ' ' + template.find('.agreement2').text(),
 								bankDetails = {
@@ -136,9 +223,9 @@ define(function(require) {
 
 							if (bankDetails.ownershipType === 'personal') {
 								bankDetails.firstName = firstname;
-								bankDetails.lastname = lastname;
+								bankDetails.lastName = lastname;
 							} else {
-								bankDetails.businessName = _.get(data, 'account.name');
+								bankDetails.businessName = _.get(args, 'data.account.name');
 							}
 
 							usBankAccountInstance.tokenize({
@@ -148,25 +235,21 @@ define(function(require) {
 								if (tokenizeErr) {
 									monster.ui.toast({
 										type: 'error',
-										message: self.i18n.active().billing.achSection.toastr.error
+										message: self.i18n.active().achDirectDebit.addFailure
 									});
 								} else {
 									monster.ui.toast({
 										type: 'success',
-										message: self.i18n.active().billing.achSection.toastr.success
+										message: self.i18n.active().achDirectDebit.addSuccess
 									});
-									self.billingRequestUpdateBilling({
+									self.putAchToken({
 										data: {
 											data: {
 												nonce: tokenizePayload.nonce
 											}
 										},
 										success: function(data) {
-											//SEND TO MICRO TRANSFER VIEW
-											var statusSection = container.find('.verification-status');
-											statusSection.text(self.i18n.active().billing.achVerification.status.pending);
-											statusSection.addClass('sds_Badge_Yellow');
-											self.achRenderVerificationSection(args);
+											self.achRenderSection(args);
 										}
 									});
 								}
@@ -187,29 +270,36 @@ define(function(require) {
 			var self = this,
 				container = args.container,
 				data = args.data,
+				bankData = data.bankData,
 				appendTemplate = function appendTemplate() {
 					var template = $(self.getTemplate({
 							name: 'ach-section-verification',
-							submodule: 'ach'
-						})),
-						verifyButton = template.find('.verify-account'),
-						enableVerifyButton = function() {
-							if (_.every(self.appFlags.ach.validAchVerificationFormFields)) {
-								verifyButton.removeClass('disabled');
-							} else {
-								verifyButton.addClass('disabled');
+							submodule: 'ach',
+							data: {
+								number: '**** **** **** ' + bankData.account_number_last_4,
+								type: _.upperFirst(bankData.account_type),
+								name: bankData.bank_name
 							}
+						})),
+						$verifyAccountButton = template.find('#verify_account'),
+						$removeAccountButton = template.find('#remove_account'),
+						enableVerifyButton = function() {
+							var isFormValid = _.every(self.appFlags.ach.validAchVerificationFormFields);
+							$verifyAccountButton.prop('disabled', !isFormValid);
 						},
 						$achForm = template.find('#form_ach_verification'),
-						billingContactData = self.appFlags.billing.billingContactFields,
-						firstname = billingContactData['contact.billing.first_name'].value,
-						lastname = billingContactData['contact.billing.last_name'].value;
+						$statusBadge = container.parents('.settings-item-content').find('#myaccount_billing_payment_ach_status');
+
+					//set badge status and text
+					$statusBadge.addClass('sds_Badge sds_Badge_Yellow');
+					$statusBadge.text(self.i18n.active().achDirectDebit.achVerification.status.pending);
 
 					container
 						.removeClass('payment-type-content-hidden')
 						.empty()
 						.append(template);
 
+					//Disable/Enable Verify button and set status badge on card
 					enableVerifyButton();
 
 					//Set validations for form
@@ -227,19 +317,211 @@ define(function(require) {
 								name = $element.attr('name'),
 								isValid = $element.valid();
 
-							self.appFlags.validAchVerificationFormFields[name] = isValid;
+							self.appFlags.ach.validAchVerificationFormFields[name] = isValid;
 
 							enableVerifyButton();
 						},
 						autoScrollOnInvalid: true
 					});
 
-					verifyButton.on('click', function(event) {
+					$verifyAccountButton.on('click', function(event) {
 						event.preventDefault();
+
+						var verificationAmounts = monster.ui.getFormData('form_ach_verification');
+
+						$verifyAccountButton.prop('disabled', true);
+						self.confirmMicroDeposits({
+							data: {
+								verificationId: _.get(bankData, 'verification_id'),
+								data: {
+									deposits: [
+										Math.floor(verificationAmounts.deposit_amount_1),
+										Math.floor(verificationAmounts.deposit_amount_2)
+									]
+								}
+							},
+							success: function(data) {
+								container.find('.ach-section-error').hide();
+								monster.ui.toast({
+									type: 'success',
+									message: self.getTemplate({
+										name: '!' + self.i18n.active().achDirectDebit.verificationSuccess,
+										data: {
+											variable: _.get(bankData, 'account_number_last_4')
+										}
+									})
+								});
+
+								self.achRenderSection(args);
+							},
+							error: function(errData) {
+								container.find('.ach-section-error').show();
+							}
+						});
+					});
+
+					$removeAccountButton.on('click', function(event) {
+						event.preventDefault();
+
+						$removeAccountButton.prop('disabled', true);
+						self.deleteAchMethod({
+							data: {
+								paymentMethodToken: bankData.payment_method_token
+							},
+							success: function(data) {
+								monster.ui.toast({
+									type: 'success',
+									message: self.i18n.active().achDirectDebit.removeSuccess
+								});
+
+								delete args.data.statusData;
+								delete args.data.bankData;
+								args.submitCallback && args.submitCallback();
+							}
+						});
 					});
 				};
 
 			appendTemplate();
+		},
+
+		achRenderShowBank: function(args) {
+			var self = this,
+				container = args.container,
+				data = args.data,
+				bankData = data.bankData,
+				appendTemplate = function appendTemplate() {
+					var template = $(self.getTemplate({
+							name: 'show-bank-account',
+							submodule: 'ach',
+							data: {
+								number: '**** **** **** ' + bankData.account_number_last_4,
+								type: _.upperFirst(bankData.account_type),
+								name: bankData.bank_name
+							}
+						})),
+						$removeAccountButton = template.find('#remove_account'),
+						$statusBadge = container.parents('.settings-item-content').find('#myaccount_billing_payment_ach_status');
+
+					//set badge status
+					$statusBadge.addClass('sds_Badge sds_Badge_Green');
+					$statusBadge.text(self.i18n.active().achDirectDebit.achVerification.status.verified);
+
+					container
+						.removeClass('payment-type-content-hidden')
+						.empty()
+						.append(template);
+
+					$removeAccountButton.on('click', function(event) {
+						event.preventDefault();
+
+						$removeAccountButton.prop('disabled', true);
+						self.deleteAchMethod({
+							data: {
+								paymentMethodToken: bankData.payment_method_token
+							},
+							success: function(data) {
+								monster.ui.toast({
+									type: 'success',
+									message: self.i18n.active().achDirectDebit.removeSuccess
+								});
+
+								delete args.data.statusData;
+								delete args.data.bankData;
+								args.submitCallback && args.submitCallback();
+							}
+						});
+					});
+				};
+
+			appendTemplate();
+		},
+
+		/*Braintree ACH functions*/
+		putAchToken: function(args) {
+			var self = this;
+
+			monster.request({
+				resource: 'myaccount.braintree.putAchToken',
+				data: _.merge({
+					accountId: self.accountId
+				}, args.data),
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
+		},
+
+		deleteAchMethod: function(args) {
+			var self = this;
+
+			monster.request({
+				resource: 'myaccount.braintree.deleteAchMethod',
+				data: _.merge({
+					accountId: self.accountId
+				}, args.data),
+
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
+		},
+
+		confirmMicroDeposits: function(args) {
+			var self = this;
+
+			monster.request({
+				resource: 'myaccount.braintree.confirmMicroDeposits',
+				data: _.merge({
+					accountId: self.accountId
+				}, args.data),
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
+		},
+
+		getVerificationStatus: function(args) {
+			var self = this;
+
+			monster.request({
+				resource: 'myaccount.braintree.getVerificationStatus',
+				data: _.merge({
+					accountId: self.accountId
+				}, args.data),
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
+		},
+
+		getBraintreeAch: function(args) {
+			var self = this;
+
+			monster.request({
+				resource: 'myaccount.braintree.getAch',
+				data: _.merge({
+					accountId: self.accountId
+				}, args.data),
+				success: function(data) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
+				}
+			});
 		}
 	};
 
